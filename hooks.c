4178bca4147adc529990d2c84ace61ced31cfbc3
高林りずむ <takabayashirizumu@takabayashirizumunoMacBook-Pro.local>
2026-06-13 21:34:14 +0900

    changes EnumServiceExA in hooks.c to comment-out advapi

--- hooks.c
+++ hooks.c
@@ -583,9 +583,9 @@ hook_t full_hooks[] = {
 	HOOK(sechost, StartServiceW),
 	HOOK(sechost, ControlService),
 	HOOK(sechost, DeleteService),
-	HOOK(advapi32, EnumServicesStatusExW),
+	//HOOK(advapi32, EnumServicesStatusExW),
 	HOOK(sechost, EnumServicesStatusExW),
-	HOOK(advapi32, EnumServicesStatusExA),
+	//HOOK(advapi32, EnumServicesStatusExA),
 	HOOK(sechost, EnumServicesStatusExA),
 
 	// Sleep Hooks
