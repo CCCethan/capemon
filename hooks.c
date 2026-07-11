a6924fdddc3be7377589bfd61e4afeb1c5356c26
CCCethan <rizumu46493@gmail.com>
2026-07-12 04:28:42 +0900

    commentout third time half of unhooked api's hooks

--- hooks.c
+++ hooks.c
@@ -875,6 +875,7 @@ hook_t full_hooks[] = {
 	HOOK(advapi32, CredFree),
 	HOOK(crypt32, CryptMsgClose),
 	HOOK(crypt32, CryptMsgGetParam),
+	/*third bisection========
 	HOOK(d3d11, D3D11CreateDevice),
 	HOOK(user32, DefWindowProc),
 	HOOK(user32, DefWindowProcA),
@@ -922,6 +923,7 @@ hook_t full_hooks[] = {
 	HOOK(kernel32, FreeEnvironmentStringsW),
 	HOOK(kernel32, FreeLibrary),
 	HOOK(iphlpapi, FreeMibTable),
+	*/============
 	/*second bisection========
 	HOOK(kernel32, GenerateConsoleCtrlEvent),
 	HOOK(kernel32, GetActiveProcessorCount),
