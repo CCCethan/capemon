1cbd2d437693016d96243d5e7d74cec0f20fcdb8
CCCethan <rizumu46493@gmail.com>
2026-07-15 05:29:39 +0900

    [eighth_bisection] S02_CreateFileMappingW-EnumSystemFirmwareTables: comment all except CreateThreadpoolTimer..CreateThreadpoolTimer (1 active / 410 added)

--- hooks.c
+++ hooks.c
@@ -865,7 +865,7 @@ hook_t full_hooks[] = {
 	//HOOK(kernel32, CreatePipe),
 	//HOOK(kernel32, CreateSemaphoreA),
 	HOOK_NOTAIL(kernel32, CreateThreadpoolTimer, 3),
-	HOOK_NOTAIL(kernel32, CreateThreadpoolWait, 3),
+	//HOOK_NOTAIL(kernel32, CreateThreadpoolWait, 3),
 	//HOOK(kernel32, CreateTimerQueue),
 	//HOOK(kernel32, CreateWaitableTimer),
 	//HOOK(kernel32, CreateWaitableTimerA),
