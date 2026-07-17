5e427339f83c454fb806d8d9b12fc9e25cd7a581
CCCethan <rizumu46493@gmail.com>
2026-07-18 08:54:36 +0900

    autohook: pa-alk_071_foreground_window_checker add generated hooks (iter 1)

--- hooks.c
+++ hooks.c
@@ -818,15 +818,15 @@ hook_t full_hooks[] = {
 	HOOK_SPECIAL(vbscript, VbsRound),
 	HOOK_SPECIAL(vbscript, VbsPrint),
 
-/* >>> AUTOHOOK_pa_alk_064_file_existence_checker BEGIN <<< */
+/* >>> AUTOHOOK_pa_alk_071_foreground_window_checker BEGIN <<< */
 HOOK(kernel32, ExitProcess),
 HOOK(kernel32, FreeLibrary),
-HOOK(kernel32, GetFileAttributesA),
+HOOK(user32, GetForegroundWindow),
 HOOK(kernel32, GetModuleFileNameW),
 HOOK(kernel32, RaiseException),
 HOOK(kernel32, Sleep),
 HOOK(kernel32, TerminateProcess),
-/* >>> AUTOHOOK_pa_alk_064_file_existence_checker END <<< */
+/* >>> AUTOHOOK_pa_alk_071_foreground_window_checker END <<< */
 };
 
 hook_t native_hooks[] = {
--- hooks.h
+++ hooks.h
@@ -3930,15 +3930,15 @@ HOOKDEF(DWORD, WINAPI, MapFileAndCheckSumA,
 
 #include "hook_vbscript.h"
 
-/* >>> AUTOHOOK_pa_alk_064_file_existence_checker BEGIN <<< */
+/* >>> AUTOHOOK_pa_alk_071_foreground_window_checker BEGIN <<< */
 HOOKDEF(VOID, WINAPI, ExitProcess,
 	_In_ UINT uExitCode
 );
 HOOKDEF(BOOL, WINAPI, FreeLibrary,
 	_In_ HMODULE hModule
 );
-HOOKDEF(DWORD, WINAPI, GetFileAttributesA,
-	_In_ LPCSTR lpFileName
+HOOKDEF(HWND, WINAPI, GetForegroundWindow,
+	void
 );
 HOOKDEF(DWORD, WINAPI, GetModuleFileNameW,
 	_In_opt_ HMODULE hModule,
@@ -3959,5 +3959,5 @@ HOOKDEF(BOOL, WINAPI, TerminateProcess,
 	_In_ UINT uExitCode
 );
 
-/* >>> AUTOHOOK_pa_alk_064_file_existence_checker END <<< */
+/* >>> AUTOHOOK_pa_alk_071_foreground_window_checker END <<< */
 
