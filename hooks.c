f37f020da9484fa1331f288a9fbc53d7cbb983fa
CCCethan <rizumu46493@gmail.com>
2026-07-18 04:50:41 +0900

    autohook: pa-alk_033_cpu_info_checker_01 add generated hooks (iter 1)

--- hooks.c
+++ hooks.c
@@ -818,20 +818,14 @@ hook_t full_hooks[] = {
 	HOOK_SPECIAL(vbscript, VbsRound),
 	HOOK_SPECIAL(vbscript, VbsPrint),
 
-/* >>> AUTOHOOK_pa_alk_021_cpu_and_disk_checker BEGIN <<< */
-HOOK(ole32, CoInitializeEx),
-HOOK(ole32, CoInitializeSecurity),
-HOOK(ole32, CoSetProxyBlanket),
-HOOK(ole32, CoUninitialize),
+/* >>> AUTOHOOK_pa_alk_033_cpu_info_checker_01 BEGIN <<< */
 HOOK(kernel32, ExitProcess),
 HOOK(kernel32, FreeLibrary),
 HOOK(kernel32, GetModuleFileNameW),
 HOOK(kernel32, RaiseException),
 HOOK(kernel32, Sleep),
-HOOK(oleaut32, SysAllocString),
 HOOK(kernel32, TerminateProcess),
-HOOK(oleaut32, VariantClear),
-/* >>> AUTOHOOK_pa_alk_021_cpu_and_disk_checker END <<< */
+/* >>> AUTOHOOK_pa_alk_033_cpu_info_checker_01 END <<< */
 };
 
 hook_t native_hooks[] = {
