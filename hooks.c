17e5194dcbdbf55a242f16bc892908cf05462a61
CCCethan <rizumu46493@gmail.com>
2026-07-19 07:41:48 +0900

    autohook: pa-alk_227_wallpaper_setting_checker add generated hooks (iter 1)

--- hooks.c
+++ hooks.c
@@ -818,15 +818,15 @@ hook_t full_hooks[] = {
 	HOOK_SPECIAL(vbscript, VbsRound),
 	HOOK_SPECIAL(vbscript, VbsPrint),
 
-/* >>> AUTOHOOK_pa_alk_212_vm_service_checker BEGIN <<< */
-HOOK(advapi32, CloseServiceHandle),
+/* >>> AUTOHOOK_pa_alk_227_wallpaper_setting_checker BEGIN <<< */
 HOOK(kernel32, ExitProcess),
 HOOK(kernel32, FreeLibrary),
 HOOK(kernel32, GetModuleFileNameW),
 HOOK(kernel32, RaiseException),
+HOOK(advapi32, RegGetValueA),
 HOOK(kernel32, Sleep),
 HOOK(kernel32, TerminateProcess),
-/* >>> AUTOHOOK_pa_alk_212_vm_service_checker END <<< */
+/* >>> AUTOHOOK_pa_alk_227_wallpaper_setting_checker END <<< */
 };
 
 hook_t native_hooks[] = {
