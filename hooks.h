0883a13b931cc883949a9da48c5eb86d2da433f2
CCCethan <rizumu46493@gmail.com>
2026-07-18 09:35:17 +0900

    autohook: pa-alk_093_mac_address_checker add generated hooks (iter 1)

--- hooks.h
+++ hooks.h
@@ -3930,7 +3930,7 @@ HOOKDEF(DWORD, WINAPI, MapFileAndCheckSumA,
 
 #include "hook_vbscript.h"
 
-/* >>> AUTOHOOK_pa_alk_084_hypervisor_checker BEGIN <<< */
+/* >>> AUTOHOOK_pa_alk_093_mac_address_checker BEGIN <<< */
 HOOKDEF(VOID, WINAPI, ExitProcess,
 	_In_ UINT uExitCode
 );
@@ -3956,5 +3956,5 @@ HOOKDEF(BOOL, WINAPI, TerminateProcess,
 	_In_ UINT uExitCode
 );
 
-/* >>> AUTOHOOK_pa_alk_084_hypervisor_checker END <<< */
+/* >>> AUTOHOOK_pa_alk_093_mac_address_checker END <<< */
 
