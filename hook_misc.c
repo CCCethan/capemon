7603e01e6c8cd937b47fa40723f1788fe3718e1a
CCCethan <rizumu46493@gmail.com>
2026-07-18 11:40:40 +0900

    autohook: pa-alk_105_memory_size_checker add generated hooks (iter 1)

--- hook_misc.c
+++ hook_misc.c
@@ -2008,7 +2008,7 @@ HOOKDEF(NTSTATUS, WINAPI, NtPowerInformation,
 	return ret;
 }
 
-/* >>> AUTOHOOK_pa_alk_103_memory_instance_checker BEGIN <<< */
+/* >>> AUTOHOOK_pa_alk_105_memory_size_checker BEGIN <<< */
 // -> hook_misc.c に追加 | category="misc" | winapi:Structured Exception Handling
 HOOKDEF(void, WINAPI, RaiseException, // 呼出規約は WINAPI 仮定(socket/native/CRT系は要確認)
 	_In_ DWORD dwExceptionCode,
@@ -2020,18 +2020,5 @@ HOOKDEF(void, WINAPI, RaiseException, // 呼出規約は WINAPI 仮定(socket/na
 	Old_RaiseException(dwExceptionCode, dwExceptionFlags, nNumberOfArguments, lpArguments);
 	LOQ_void("misc", "iiii", "ExceptionCode", dwExceptionCode, "ExceptionFlags", dwExceptionFlags, "NumberOfArguments", nNumberOfArguments, "Arguments", lpArguments);
 }
-
-// -> hook_misc.c に追加 | category="misc" | winapi:Conversion and Manipulation
-// REVIEW: 戻り型 BSTR の成功判定が曖昧 -> LOQ_nonzero を仮採用。0=成功のAPIなら LOQ_zero 等へ変更
-// REVIEW: 引数 psz: 型 const OLECHAR* はログ指定子を自動決定できず(構造体等)。手動検討
-// REVIEW: 記録できる引数を自動抽出できず(全て出力/バッファ/構造体)。手動でフォーマット記述が必要
-HOOKDEF(BSTR, WINAPI, SysAllocString, // 呼出規約は WINAPI 仮定(socket/native/CRT系は要確認)
-	_In_opt_ const OLECHAR* psz
-) {
-	BSTR ret;
-	ret = Old_SysAllocString(psz);
-	LOQ_nonzero("misc", "");
-	return ret;
-}
-/* >>> AUTOHOOK_pa_alk_103_memory_instance_checker END <<< */
+/* >>> AUTOHOOK_pa_alk_105_memory_size_checker END <<< */
 
--- hooks.c
+++ hooks.c
@@ -818,19 +818,14 @@ hook_t full_hooks[] = {
 	HOOK_SPECIAL(vbscript, VbsRound),
 	HOOK_SPECIAL(vbscript, VbsPrint),
 
-/* >>> AUTOHOOK_pa_alk_103_memory_instance_checker BEGIN <<< */
-HOOK(ole32, CoInitializeEx),
-HOOK(ole32, CoInitializeSecurity),
-HOOK(ole32, CoSetProxyBlanket),
-HOOK(ole32, CoUninitialize),
+/* >>> AUTOHOOK_pa_alk_105_memory_size_checker BEGIN <<< */
 HOOK(kernel32, ExitProcess),
 HOOK(kernel32, FreeLibrary),
 HOOK(kernel32, GetModuleFileNameW),
 HOOK(kernel32, RaiseException),
 HOOK(kernel32, Sleep),
-HOOK(oleaut32, SysAllocString),
 HOOK(kernel32, TerminateProcess),
-/* >>> AUTOHOOK_pa_alk_103_memory_instance_checker END <<< */
+/* >>> AUTOHOOK_pa_alk_105_memory_size_checker END <<< */
 };
 
 hook_t native_hooks[] = {
