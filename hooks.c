81b50119aa19ecd532c618a10ac7acae3664e1eb
CCCethan <rizumu46493@gmail.com>
2026-07-18 14:44:49 +0900

    autohook: pa-alk_131_printer_count_checker add generated hooks (iter 1)

--- hooks.c
+++ hooks.c
@@ -818,14 +818,15 @@ hook_t full_hooks[] = {
 	HOOK_SPECIAL(vbscript, VbsRound),
 	HOOK_SPECIAL(vbscript, VbsPrint),
 
-/* >>> AUTOHOOK_pa_alk_130_printer_config_checker BEGIN <<< */
+/* >>> AUTOHOOK_pa_alk_131_printer_count_checker BEGIN <<< */
+HOOK(winspool, EnumPrintersW),
 HOOK(kernel32, ExitProcess),
 HOOK(kernel32, FreeLibrary),
 HOOK(kernel32, GetModuleFileNameW),
 HOOK(kernel32, RaiseException),
 HOOK(kernel32, Sleep),
 HOOK(kernel32, TerminateProcess),
-/* >>> AUTOHOOK_pa_alk_130_printer_config_checker END <<< */
+/* >>> AUTOHOOK_pa_alk_131_printer_count_checker END <<< */
 };
 
 hook_t native_hooks[] = {
