a67f1ceaaea83d39056adea288634283a075f062
CCCethan <rizumu46493@gmail.com>
2026-07-18 20:14:13 +0900

    autohook: pa-alk_173_system_uptime_checker_01 add generated hooks (iter 1)

--- hooks.h
+++ hooks.h
@@ -3930,7 +3930,7 @@ HOOKDEF(DWORD, WINAPI, MapFileAndCheckSumA,
 
 #include "hook_vbscript.h"
 
-/* >>> AUTOHOOK_pa_alk_172_system_uptime_checker BEGIN <<< */
+/* >>> AUTOHOOK_pa_alk_173_system_uptime_checker_01 BEGIN <<< */
 HOOKDEF(VOID, WINAPI, ExitProcess,
 	_In_ UINT uExitCode
 );
@@ -3956,5 +3956,5 @@ HOOKDEF(BOOL, WINAPI, TerminateProcess,
 	_In_ UINT uExitCode
 );
 
-/* >>> AUTOHOOK_pa_alk_172_system_uptime_checker END <<< */
+/* >>> AUTOHOOK_pa_alk_173_system_uptime_checker_01 END <<< */
 
