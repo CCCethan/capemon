e290b84b0cc59bab288301ec281854ed5fedbd03
CCCethan <rizumu46493@gmail.com>
2026-07-19 00:17:02 +0900

    autohook: pa-alk_198_virtual_process_checker add generated hooks (iter 1)

--- hooks.h
+++ hooks.h
@@ -3930,7 +3930,7 @@ HOOKDEF(DWORD, WINAPI, MapFileAndCheckSumA,
 
 #include "hook_vbscript.h"
 
-/* >>> AUTOHOOK_pa_alk_190_vbox_window_checker BEGIN <<< */
+/* >>> AUTOHOOK_pa_alk_198_virtual_process_checker BEGIN <<< */
 HOOKDEF(VOID, WINAPI, ExitProcess,
 	_In_ UINT uExitCode
 );
@@ -3956,5 +3956,5 @@ HOOKDEF(BOOL, WINAPI, TerminateProcess,
 	_In_ UINT uExitCode
 );
 
-/* >>> AUTOHOOK_pa_alk_190_vbox_window_checker END <<< */
+/* >>> AUTOHOOK_pa_alk_198_virtual_process_checker END <<< */
 
