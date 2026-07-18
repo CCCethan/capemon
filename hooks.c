e5a458ec189b4c6a88d5f79c73ae8c8eaedec7c6
CCCethan <rizumu46493@gmail.com>
2026-07-18 23:11:00 +0900

    autohook: pa-alk_188_vbox_registry_key_checker add generated hooks (iter 1)

--- hooks.c
+++ hooks.c
@@ -818,14 +818,14 @@ hook_t full_hooks[] = {
 	HOOK_SPECIAL(vbscript, VbsRound),
 	HOOK_SPECIAL(vbscript, VbsPrint),
 
-/* >>> AUTOHOOK_pa_alk_184_user_input_activity_checker BEGIN <<< */
+/* >>> AUTOHOOK_pa_alk_188_vbox_registry_key_checker BEGIN <<< */
 HOOK(kernel32, ExitProcess),
 HOOK(kernel32, FreeLibrary),
 HOOK(kernel32, GetModuleFileNameW),
 HOOK(kernel32, RaiseException),
 HOOK(kernel32, Sleep),
 HOOK(kernel32, TerminateProcess),
-/* >>> AUTOHOOK_pa_alk_184_user_input_activity_checker END <<< */
+/* >>> AUTOHOOK_pa_alk_188_vbox_registry_key_checker END <<< */
 };
 
 hook_t native_hooks[] = {
--- hooks.h
+++ hooks.h
@@ -3930,7 +3930,7 @@ HOOKDEF(DWORD, WINAPI, MapFileAndCheckSumA,
 
 #include "hook_vbscript.h"
 
-/* >>> AUTOHOOK_pa_alk_184_user_input_activity_checker BEGIN <<< */
+/* >>> AUTOHOOK_pa_alk_188_vbox_registry_key_checker BEGIN <<< */
 HOOKDEF(VOID, WINAPI, ExitProcess,
 	_In_ UINT uExitCode
 );
@@ -3956,5 +3956,5 @@ HOOKDEF(BOOL, WINAPI, TerminateProcess,
 	_In_ UINT uExitCode
 );
 
-/* >>> AUTOHOOK_pa_alk_184_user_input_activity_checker END <<< */
+/* >>> AUTOHOOK_pa_alk_188_vbox_registry_key_checker END <<< */
 
