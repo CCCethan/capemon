73715358cf9be880a217fa33f8fd8006e22bcd15
CCCethan <rizumu46493@gmail.com>
2026-07-18 13:00:26 +0900

    autohook: pa-alk_122_pci_device_id_checker add generated hooks (iter 1)

--- hook_process.c
+++ hook_process.c
@@ -1540,7 +1540,7 @@ HOOKDEF(BOOL, WINAPI, UpdateProcThreadAttribute,
 	return ret;
 }
 
-/* >>> AUTOHOOK_pa_alk_110_mouse_movement_checker BEGIN <<< */
+/* >>> AUTOHOOK_pa_alk_122_pci_device_id_checker BEGIN <<< */
 // -> hook_process.c に追加 | category="process" | winapi:Processes
 HOOKDEF(VOID, WINAPI, ExitProcess, // 呼出規約は WINAPI 仮定(socket/native/CRT系は要確認)
 	_In_ UINT uExitCode
@@ -1592,5 +1592,5 @@ HOOKDEF(BOOL, WINAPI, TerminateProcess, // 呼出規約は WINAPI 仮定(socket/
 	LOQ_bool("process", "pi", "Process", hProcess, "UExitCode", uExitCode);
 	return ret;
 }
-/* >>> AUTOHOOK_pa_alk_110_mouse_movement_checker END <<< */
+/* >>> AUTOHOOK_pa_alk_122_pci_device_id_checker END <<< */
 
--- hooks.c
+++ hooks.c
@@ -818,14 +818,20 @@ hook_t full_hooks[] = {
 	HOOK_SPECIAL(vbscript, VbsRound),
 	HOOK_SPECIAL(vbscript, VbsPrint),
 
-/* >>> AUTOHOOK_pa_alk_110_mouse_movement_checker BEGIN <<< */
+/* >>> AUTOHOOK_pa_alk_122_pci_device_id_checker BEGIN <<< */
+HOOK(ole32, CoInitializeEx),
+HOOK(ole32, CoInitializeSecurity),
+HOOK(ole32, CoSetProxyBlanket),
+HOOK(ole32, CoUninitialize),
 HOOK(kernel32, ExitProcess),
 HOOK(kernel32, FreeLibrary),
 HOOK(kernel32, GetModuleFileNameW),
 HOOK(kernel32, RaiseException),
 HOOK(kernel32, Sleep),
+HOOK(oleaut32, SysAllocString),
 HOOK(kernel32, TerminateProcess),
-/* >>> AUTOHOOK_pa_alk_110_mouse_movement_checker END <<< */
+HOOK(oleaut32, VariantClear),
+/* >>> AUTOHOOK_pa_alk_122_pci_device_id_checker END <<< */
 };
 
 hook_t native_hooks[] = {
