0398764c75d9ab3cf43d14ce4bb85b2e719f5dd4
CCCethan <rizumu46493@gmail.com>
2026-07-18 09:50:42 +0900

    autohook: pa-alk_098_mac_address_prefix_checker add generated hooks (iter 1)

--- hook_process.c
+++ hook_process.c
@@ -1540,7 +1540,7 @@ HOOKDEF(BOOL, WINAPI, UpdateProcThreadAttribute,
 	return ret;
 }
 
-/* >>> AUTOHOOK_pa_alk_093_mac_address_checker BEGIN <<< */
+/* >>> AUTOHOOK_pa_alk_098_mac_address_prefix_checker BEGIN <<< */
 // -> hook_process.c に追加 | category="process" | winapi:Processes
 HOOKDEF(VOID, WINAPI, ExitProcess, // 呼出規約は WINAPI 仮定(socket/native/CRT系は要確認)
 	_In_ UINT uExitCode
@@ -1592,5 +1592,5 @@ HOOKDEF(BOOL, WINAPI, TerminateProcess, // 呼出規約は WINAPI 仮定(socket/
 	LOQ_bool("process", "pi", "Process", hProcess, "UExitCode", uExitCode);
 	return ret;
 }
-/* >>> AUTOHOOK_pa_alk_093_mac_address_checker END <<< */
+/* >>> AUTOHOOK_pa_alk_098_mac_address_prefix_checker END <<< */
 
