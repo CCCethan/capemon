6aac140f0448eab89c3871273f727bcc09f0563a
CCCethan <rizumu46493@gmail.com>
2026-07-19 05:03:14 +0900

    autohook: pa-alk_209_virtualbox_window_checker add generated hooks (iter 1)

--- hooks.c
+++ hooks.c
@@ -818,17 +818,14 @@ hook_t full_hooks[] = {
 	HOOK_SPECIAL(vbscript, VbsRound),
 	HOOK_SPECIAL(vbscript, VbsPrint),
 
-/* >>> AUTOHOOK_pa_alk_208_virtualbox_shared_folder_checker BEGIN <<< */
+/* >>> AUTOHOOK_pa_alk_209_virtualbox_window_checker BEGIN <<< */
 HOOK(kernel32, ExitProcess),
 HOOK(kernel32, FreeLibrary),
 HOOK(kernel32, GetModuleFileNameW),
 HOOK(kernel32, RaiseException),
 HOOK(kernel32, Sleep),
 HOOK(kernel32, TerminateProcess),
-HOOK(mpr, WNetCloseEnum),
-HOOK(mpr, WNetEnumResourceW),
-HOOK(mpr, WNetOpenEnumW),
-/* >>> AUTOHOOK_pa_alk_208_virtualbox_shared_folder_checker END <<< */
+/* >>> AUTOHOOK_pa_alk_209_virtualbox_window_checker END <<< */
 };
 
 hook_t native_hooks[] = {
