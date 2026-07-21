5711444884974d43428797b079d8b903b41c3e1a
CCCethan <rizumu46493@gmail.com>
2026-07-22 08:14:26 +0900

    autohook: pa-alk_133_process_based_env_checker add generated hooks (iter 1)

--- hooks.c
+++ hooks.c
@@ -818,11 +818,9 @@ hook_t full_hooks[] = {
 	HOOK_SPECIAL(vbscript, VbsRound),
 	HOOK_SPECIAL(vbscript, VbsPrint),
 
-/* >>> AUTOHOOK_pa_alk_132_printer_presence_checker BEGIN <<< */
+/* >>> AUTOHOOK_pa_alk_133_process_based_env_checker BEGIN <<< */
 HOOK(kernel32, AreFileApisANSI),
 HOOK(kernelbase, AreFileApisANSI),
-HOOK(winspool, EnumPrintersA),
-HOOK(winspool.drv, EnumPrintersA),
 HOOK(kernel32, ExitProcess),
 HOOK(kernelbase, ExitProcess),
 HOOK(kernel32, FreeLibrary),
@@ -837,7 +835,7 @@ HOOK(kernel32, Sleep),
 HOOK(kernelbase, Sleep),
 HOOK(kernel32, TerminateProcess),
 HOOK(kernelbase, TerminateProcess),
-/* >>> AUTOHOOK_pa_alk_132_printer_presence_checker END <<< */
+/* >>> AUTOHOOK_pa_alk_133_process_based_env_checker END <<< */
 };
 
 hook_t native_hooks[] = {
--- hooks.h
+++ hooks.h
@@ -3930,18 +3930,9 @@ HOOKDEF(DWORD, WINAPI, MapFileAndCheckSumA,
 
 #include "hook_vbscript.h"
 
-/* >>> AUTOHOOK_pa_alk_132_printer_presence_checker BEGIN <<< */
+/* >>> AUTOHOOK_pa_alk_133_process_based_env_checker BEGIN <<< */
 HOOKDEF(BOOL, WINAPI, AreFileApisANSI,
 	void
-);
-HOOKDEF(BOOL, WINAPI, EnumPrintersA,
-	_In_ DWORD Flags,
-	_In_ LPSTR Name,
-	_In_ DWORD Level,
-	_Out_ LPBYTE pPrinterEnum,
-	_In_ DWORD cbBuf,
-	_Out_ LPDWORD pcbNeeded,
-	_Out_ LPDWORD pcReturned
 );
 HOOKDEF(VOID, WINAPI, ExitProcess,
 	_In_ UINT uExitCode
@@ -3971,5 +3962,5 @@ HOOKDEF(BOOL, WINAPI, TerminateProcess,
 	_In_ UINT uExitCode
 );
 
-/* >>> AUTOHOOK_pa_alk_132_printer_presence_checker END <<< */
+/* >>> AUTOHOOK_pa_alk_133_process_based_env_checker END <<< */
 
