a7f86f3fb0e4ae892c693cbea4954ed4ccf580b1
CCCethan <rizumu46493@gmail.com>
2026-07-22 06:20:45 +0900

    autohook: pa-alk_122_pci_device_id_checker add generated hooks (iter 1)

--- hooks.c
+++ hooks.c
@@ -818,7 +818,7 @@ hook_t full_hooks[] = {
 	HOOK_SPECIAL(vbscript, VbsRound),
 	HOOK_SPECIAL(vbscript, VbsPrint),
 
-/* >>> AUTOHOOK_pa_alk_110_mouse_movement_checker BEGIN <<< */
+/* >>> AUTOHOOK_pa_alk_122_pci_device_id_checker BEGIN <<< */
 HOOK(kernel32, AreFileApisANSI),
 HOOK(kernelbase, AreFileApisANSI),
 HOOK(kernel32, ExitProcess),
@@ -835,7 +835,7 @@ HOOK(kernel32, Sleep),
 HOOK(kernelbase, Sleep),
 HOOK(kernel32, TerminateProcess),
 HOOK(kernelbase, TerminateProcess),
-/* >>> AUTOHOOK_pa_alk_110_mouse_movement_checker END <<< */
+/* >>> AUTOHOOK_pa_alk_122_pci_device_id_checker END <<< */
 };
 
 hook_t native_hooks[] = {
