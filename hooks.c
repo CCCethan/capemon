68775f4764da86b498a1254493d3a91c1df99cb4
CCCethan <rizumu46493@gmail.com>
2026-07-22 06:02:06 +0900

    autohook: pa-alk_110_mouse_movement_checker add generated hooks (iter 1)

--- hooks.c
+++ hooks.c
@@ -818,11 +818,9 @@ hook_t full_hooks[] = {
 	HOOK_SPECIAL(vbscript, VbsRound),
 	HOOK_SPECIAL(vbscript, VbsPrint),
 
-/* >>> AUTOHOOK_pa_alk_109_mouse_event_checker BEGIN <<< */
+/* >>> AUTOHOOK_pa_alk_110_mouse_movement_checker BEGIN <<< */
 HOOK(kernel32, AreFileApisANSI),
 HOOK(kernelbase, AreFileApisANSI),
-HOOK(user32, CallNextHookEx),
-HOOK(user32, DispatchMessageW),
 HOOK(kernel32, ExitProcess),
 HOOK(kernelbase, ExitProcess),
 HOOK(kernel32, FreeLibrary),
@@ -831,17 +829,13 @@ HOOK(kernel32, GetModuleFileNameW),
 HOOK(kernelbase, GetModuleFileNameW),
 HOOK(kernel32, IsProcessorFeaturePresent),
 HOOK(kernelbase, IsProcessorFeaturePresent),
-HOOK(user32, MsgWaitForMultipleObjects),
-HOOK(user32, PeekMessageW),
-HOOK(user32, PostQuitMessage),
 HOOK(kernel32, RaiseException),
 HOOK(kernelbase, RaiseException),
 HOOK(kernel32, Sleep),
 HOOK(kernelbase, Sleep),
 HOOK(kernel32, TerminateProcess),
 HOOK(kernelbase, TerminateProcess),
-HOOK(user32, TranslateMessage),
-/* >>> AUTOHOOK_pa_alk_109_mouse_event_checker END <<< */
+/* >>> AUTOHOOK_pa_alk_110_mouse_movement_checker END <<< */
 };
 
 hook_t native_hooks[] = {
