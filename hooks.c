3228d1ccd0243d7d482d56415993dcf3e8ccc102
CCCethan <rizumu46493@gmail.com>
2026-07-22 00:38:36 +0900

    autohook: pa-alk_035_cpu_processor_count_checker add generated hooks (iter 1)

--- hooks.c
+++ hooks.c
@@ -818,7 +818,7 @@ hook_t full_hooks[] = {
 	HOOK_SPECIAL(vbscript, VbsRound),
 	HOOK_SPECIAL(vbscript, VbsPrint),
 
-/* >>> AUTOHOOK_pa_alk_034_cpu_info_checker_02 BEGIN <<< */
+/* >>> AUTOHOOK_pa_alk_035_cpu_processor_count_checker BEGIN <<< */
 HOOK(kernel32, AreFileApisANSI),
 HOOK(kernelbase, AreFileApisANSI),
 HOOK(kernel32, ExitProcess),
@@ -835,7 +835,7 @@ HOOK(kernel32, Sleep),
 HOOK(kernelbase, Sleep),
 HOOK(kernel32, TerminateProcess),
 HOOK(kernelbase, TerminateProcess),
-/* >>> AUTOHOOK_pa_alk_034_cpu_info_checker_02 END <<< */
+/* >>> AUTOHOOK_pa_alk_035_cpu_processor_count_checker END <<< */
 };
 
 hook_t native_hooks[] = {
