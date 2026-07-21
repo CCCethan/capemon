3967a11c428828d422cd5726592ed70f62308023
CCCethan <rizumu46493@gmail.com>
2026-07-21 22:44:54 +0900

    autohook: pa-alk_015_cloud_storage_environment_checker add generated hooks (iter 1)

--- hooks.c
+++ hooks.c
@@ -818,13 +818,25 @@ hook_t full_hooks[] = {
 	HOOK_SPECIAL(vbscript, VbsRound),
 	HOOK_SPECIAL(vbscript, VbsPrint),
 
-/* >>> AUTOHOOK_pa_alk_008_bluetooth_device_checker BEGIN <<< */
+/* >>> AUTOHOOK_pa_alk_015_cloud_storage_environment_checker BEGIN <<< */
 HOOK(kernel32, AreFileApisANSI),
 HOOK(kernelbase, AreFileApisANSI),
+HOOK(kernel32, CreatePipe),
+HOOK(kernelbase, CreatePipe),
+HOOK(kernel32, DuplicateHandle),
+HOOK(kernelbase, DuplicateHandle),
 HOOK(kernel32, ExitProcess),
 HOOK(kernelbase, ExitProcess),
+HOOK(kernel32, FormatMessageA),
+HOOK(kernelbase, FormatMessageA),
 HOOK(kernel32, FreeLibrary),
 HOOK(kernelbase, FreeLibrary),
+HOOK(kernel32, GetExitCodeProcess),
+HOOK(kernelbase, GetExitCodeProcess),
+HOOK(kernel32, GetFileAttributesExW),
+HOOK(kernelbase, GetFileAttributesExW),
+HOOK(kernel32, GetFileInformationByHandleEx),
+HOOK(kernelbase, GetFileInformationByHandleEx),
 HOOK(kernel32, GetModuleFileNameW),
 HOOK(kernelbase, GetModuleFileNameW),
 HOOK(kernel32, IsProcessorFeaturePresent),
@@ -835,7 +847,9 @@ HOOK(kernel32, Sleep),
 HOOK(kernelbase, Sleep),
 HOOK(kernel32, TerminateProcess),
 HOOK(kernelbase, TerminateProcess),
-/* >>> AUTOHOOK_pa_alk_008_bluetooth_device_checker END <<< */
+HOOK(kernel32, WaitForSingleObject),
+HOOK(kernelbase, WaitForSingleObject),
+/* >>> AUTOHOOK_pa_alk_015_cloud_storage_environment_checker END <<< */
 };
 
 hook_t native_hooks[] = {
