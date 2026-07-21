614e33874c7393a2f87ddf8d34e5a372fc954843
CCCethan <rizumu46493@gmail.com>
2026-07-22 05:05:38 +0900

    autohook: pa-alk_105_memory_size_checker add generated hooks (iter 1)

--- hooks.c
+++ hooks.c
@@ -818,7 +818,7 @@ hook_t full_hooks[] = {
 	HOOK_SPECIAL(vbscript, VbsRound),
 	HOOK_SPECIAL(vbscript, VbsPrint),
 
-/* >>> AUTOHOOK_pa_alk_103_memory_instance_checker BEGIN <<< */
+/* >>> AUTOHOOK_pa_alk_105_memory_size_checker BEGIN <<< */
 HOOK(kernel32, AreFileApisANSI),
 HOOK(kernelbase, AreFileApisANSI),
 HOOK(kernel32, ExitProcess),
@@ -835,7 +835,7 @@ HOOK(kernel32, Sleep),
 HOOK(kernelbase, Sleep),
 HOOK(kernel32, TerminateProcess),
 HOOK(kernelbase, TerminateProcess),
-/* >>> AUTOHOOK_pa_alk_103_memory_instance_checker END <<< */
+/* >>> AUTOHOOK_pa_alk_105_memory_size_checker END <<< */
 };
 
 hook_t native_hooks[] = {
--- hooks.h
+++ hooks.h
@@ -3930,7 +3930,7 @@ HOOKDEF(DWORD, WINAPI, MapFileAndCheckSumA,
 
 #include "hook_vbscript.h"
 
-/* >>> AUTOHOOK_pa_alk_103_memory_instance_checker BEGIN <<< */
+/* >>> AUTOHOOK_pa_alk_105_memory_size_checker BEGIN <<< */
 HOOKDEF(BOOL, WINAPI, AreFileApisANSI,
 	void
 );
@@ -3962,5 +3962,5 @@ HOOKDEF(BOOL, WINAPI, TerminateProcess,
 	_In_ UINT uExitCode
 );
 
-/* >>> AUTOHOOK_pa_alk_103_memory_instance_checker END <<< */
+/* >>> AUTOHOOK_pa_alk_105_memory_size_checker END <<< */
 
