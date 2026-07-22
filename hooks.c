eae6ffc7c4b582fcb1c91bdf1b4339ccadeac105
CCCethan <rizumu46493@gmail.com>
2026-07-22 10:46:53 +0900

    autohook: pa-alk_064_file_existence_checker add generated hooks (iter 1)

--- hooks.c
+++ hooks.c
@@ -818,17 +818,15 @@ hook_t full_hooks[] = {
 	HOOK_SPECIAL(vbscript, VbsRound),
 	HOOK_SPECIAL(vbscript, VbsPrint),
 
-/* >>> AUTOHOOK_pa_alk_061_error_report_checker BEGIN <<< */
+/* >>> AUTOHOOK_pa_alk_064_file_existence_checker BEGIN <<< */
 HOOK(kernel32, AreFileApisANSI),
 HOOK(kernelbase, AreFileApisANSI),
 HOOK(kernel32, ExitProcess),
 HOOK(kernelbase, ExitProcess),
 HOOK(kernel32, FreeLibrary),
 HOOK(kernelbase, FreeLibrary),
-HOOK(kernel32, GetFileAttributesExW),
-HOOK(kernelbase, GetFileAttributesExW),
-HOOK(kernel32, GetFileInformationByHandleEx),
-HOOK(kernelbase, GetFileInformationByHandleEx),
+HOOK(kernel32, GetFileAttributesA),
+HOOK(kernelbase, GetFileAttributesA),
 HOOK(kernel32, GetModuleFileNameW),
 HOOK(kernelbase, GetModuleFileNameW),
 HOOK(kernel32, IsProcessorFeaturePresent),
@@ -839,7 +837,7 @@ HOOK(kernel32, Sleep),
 HOOK(kernelbase, Sleep),
 HOOK(kernel32, TerminateProcess),
 HOOK(kernelbase, TerminateProcess),
-/* >>> AUTOHOOK_pa_alk_061_error_report_checker END <<< */
+/* >>> AUTOHOOK_pa_alk_064_file_existence_checker END <<< */
 };
 
 hook_t native_hooks[] = {
