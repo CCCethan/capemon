9bc8d8f0e752816befc15653893f8713e43152ff
CCCethan <rizumu46493@gmail.com>
2026-07-23 00:15:37 +0900

    autohook: pa-alk_064_file_existence_checker add generated hooks (iter 1)

--- hooks.h
+++ hooks.h
@@ -3930,7 +3930,7 @@ HOOKDEF(DWORD, WINAPI, MapFileAndCheckSumA,
 
 #include "hook_vbscript.h"
 
-/* >>> AUTOHOOK_pa_alk_235_wmi_system_model_checker BEGIN <<< */
+/* >>> AUTOHOOK_pa_alk_064_file_existence_checker BEGIN <<< */
 HOOKDEF(BOOL, WINAPI, AreFileApisANSI,
 	void
 );
@@ -3962,5 +3962,5 @@ HOOKDEF(BOOL, WINAPI, TerminateProcess,
 	_In_ UINT uExitCode
 );
 
-/* >>> AUTOHOOK_pa_alk_235_wmi_system_model_checker END <<< */
+/* >>> AUTOHOOK_pa_alk_064_file_existence_checker END <<< */
 
