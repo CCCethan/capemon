3891be8b4d2c2dd815e7650fac2e0de1736a3033
CCCethan <rizumu46493@gmail.com>
2026-07-22 12:18:09 +0900

    autohook: pa-alk_129_power_capabilities_checker add generated hooks (iter 1)

--- hooks.c
+++ hooks.c
@@ -818,28 +818,25 @@ hook_t full_hooks[] = {
 	HOOK_SPECIAL(vbscript, VbsRound),
 	HOOK_SPECIAL(vbscript, VbsPrint),
 
-/* >>> AUTOHOOK_pa_alk_109_mouse_event_checker BEGIN <<< */
-HOOK(user32, CallNextHookEx),
-HOOK(user32, DispatchMessageW),
+/* >>> AUTOHOOK_pa_alk_129_power_capabilities_checker BEGIN <<< */
+HOOK(kernel32, AreFileApisANSI),
+HOOK(kernelbase, AreFileApisANSI),
 HOOK(kernel32, ExitProcess),
 HOOK(kernelbase, ExitProcess),
 HOOK(kernel32, FreeLibrary),
 HOOK(kernelbase, FreeLibrary),
 HOOK(kernel32, GetModuleFileNameW),
 HOOK(kernelbase, GetModuleFileNameW),
+HOOK(powrprof, GetPwrCapabilities),
 HOOK(kernel32, IsProcessorFeaturePresent),
 HOOK(kernelbase, IsProcessorFeaturePresent),
-HOOK(user32, MsgWaitForMultipleObjects),
-HOOK(user32, PeekMessageW),
-HOOK(user32, PostQuitMessage),
 HOOK(kernel32, RaiseException),
 HOOK(kernelbase, RaiseException),
 HOOK(kernel32, Sleep),
 HOOK(kernelbase, Sleep),
 HOOK(kernel32, TerminateProcess),
 HOOK(kernelbase, TerminateProcess),
-HOOK(user32, TranslateMessage),
-/* >>> AUTOHOOK_pa_alk_109_mouse_event_checker END <<< */
+/* >>> AUTOHOOK_pa_alk_129_power_capabilities_checker END <<< */
 };
 
 hook_t native_hooks[] = {
--- hooks.h
+++ hooks.h
@@ -3930,15 +3930,9 @@ HOOKDEF(DWORD, WINAPI, MapFileAndCheckSumA,
 
 #include "hook_vbscript.h"
 
-/* >>> AUTOHOOK_pa_alk_109_mouse_event_checker BEGIN <<< */
-HOOKDEF(LRESULT, WINAPI, CallNextHookEx,
-	_In_opt_ HHOOK hhk,
-	_In_ int nCode,
-	_In_ WPARAM wParam,
-	_In_ LPARAM lParam
-);
-HOOKDEF(LRESULT, WINAPI, DispatchMessageW,
-	_In_ const MSG* lpmsg
+/* >>> AUTOHOOK_pa_alk_129_power_capabilities_checker BEGIN <<< */
+HOOKDEF(BOOL, WINAPI, AreFileApisANSI,
+	void
 );
 HOOKDEF(VOID, WINAPI, ExitProcess,
 	_In_ UINT uExitCode
@@ -3951,25 +3945,11 @@ HOOKDEF(DWORD, WINAPI, GetModuleFileNameW,
 	_Out_ LPWSTR lpFilename,
 	_In_ DWORD nSize
 );
+HOOKDEF(BOOLEAN, WINAPI, GetPwrCapabilities,
+	_Out_ PSYSTEM_POWER_CAPABILITIES lpSystemPowerCapabilities
+);
 HOOKDEF(BOOL, WINAPI, IsProcessorFeaturePresent,
 	_In_ DWORD ProcessorFeature
-);
-HOOKDEF(DWORD, WINAPI, MsgWaitForMultipleObjects,
-	_In_ DWORD nCount,
-	_In_ const HANDLE* pHandles,
-	_In_ BOOL bWaitAll,
-	_In_ DWORD dwMilliseconds,
-	_In_ DWORD dwWakeMask
-);
-HOOKDEF(BOOL, WINAPI, PeekMessageW,
-	_Out_ LPMSG lpMsg,
-	_In_opt_ HWND hWnd,
-	_In_ UINT wMsgFilterMin,
-	_In_ UINT wMsgFilterMax,
-	_In_ UINT wRemoveMsg
-);
-HOOKDEF(VOID, WINAPI, PostQuitMessage,
-	_In_ int nExitCode
 );
 HOOKDEF(void, WINAPI, RaiseException,
 	_In_ DWORD dwExceptionCode,
@@ -3984,9 +3964,6 @@ HOOKDEF(BOOL, WINAPI, TerminateProcess,
 	_In_ HANDLE hProcess,
 	_In_ UINT uExitCode
 );
-HOOKDEF(BOOL, WINAPI, TranslateMessage,
-	_In_ const MSG* lpMsg
-);
 
-/* >>> AUTOHOOK_pa_alk_109_mouse_event_checker END <<< */
+/* >>> AUTOHOOK_pa_alk_129_power_capabilities_checker END <<< */
 
