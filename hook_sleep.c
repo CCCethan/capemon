b13254e3d91669ac8fc362f34485935388761a89
CCCethan <rizumu46493@gmail.com>
2026-07-22 14:10:20 +0900

    autohook: pa-alk_173_system_uptime_checker_01 add generated hooks (iter 1)

--- hook_sleep.c
+++ hook_sleep.c
@@ -541,8 +541,6 @@ HOOKDEF(DWORD, WINAPI, GetTickCount,
 	if (sleep_skip_active)
 		ret += (DWORD)(time_skipped.QuadPart / 10000);
 
-	/* AUTOHOOK augment: 既存フックに LOQ を追記(機能は維持) */
-	LOQ_nonzero("misc", "");
 	return ret;
 }
 
@@ -559,6 +557,8 @@ HOOKDEF(ULONGLONG, WINAPI, GetTickCount64,
 	if (sleep_skip_active)
 		ret += (time_skipped.QuadPart / 10000);
 
+	/* AUTOHOOK augment: 既存フックに LOQ を追記(機能は維持) */
+	LOQ_nonzero("misc", "");
 	return ret;
 }
 
