a7c26101f9b8c9a2d4637dbc969df2a9a35e6861
CCCethan <rizumu46493@gmail.com>
2026-07-22 09:30:30 +0900

    autohook: pa-alk_015_cloud_storage_environment_checker add generated hooks (iter 1)

--- hooks.c
+++ hooks.c
@@ -818,13 +818,17 @@ hook_t full_hooks[] = {
 	HOOK_SPECIAL(vbscript, VbsRound),
 	HOOK_SPECIAL(vbscript, VbsPrint),
 
-/* >>> AUTOHOOK_pa_alk_004_baseboard_info_checker BEGIN <<< */
+/* >>> AUTOHOOK_pa_alk_015_cloud_storage_environment_checker BEGIN <<< */
 HOOK(kernel32, AreFileApisANSI),
 HOOK(kernelbase, AreFileApisANSI),
 HOOK(kernel32, ExitProcess),
 HOOK(kernelbase, ExitProcess),
 HOOK(kernel32, FreeLibrary),
 HOOK(kernelbase, FreeLibrary),
+HOOK(kernel32, GetFileAttributesExW),
+HOOK(kernelbase, GetFileAttributesExW),
+HOOK(kernel32, GetFileInformationByHandleEx),
+HOOK(kernelbase, GetFileInformationByHandleEx),
 HOOK(kernel32, GetModuleFileNameW),
 HOOK(kernelbase, GetModuleFileNameW),
 HOOK(kernel32, IsProcessorFeaturePresent),
@@ -835,7 +839,7 @@ HOOK(kernel32, Sleep),
 HOOK(kernelbase, Sleep),
 HOOK(kernel32, TerminateProcess),
 HOOK(kernelbase, TerminateProcess),
-/* >>> AUTOHOOK_pa_alk_004_baseboard_info_checker END <<< */
+/* >>> AUTOHOOK_pa_alk_015_cloud_storage_environment_checker END <<< */
 };
 
 hook_t native_hooks[] = {
