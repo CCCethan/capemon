371972594ddc5a185af0153591645cdeca56ea65
CCCethan <rizumu46493@gmail.com>
2026-07-22 19:27:35 +0900

    autohook: pa-alk_183_user_inactivity_checker add generated hooks (iter 1)

--- hooks.h
+++ hooks.h
@@ -3930,7 +3930,10 @@ HOOKDEF(DWORD, WINAPI, MapFileAndCheckSumA,
 
 #include "hook_vbscript.h"
 
-/* >>> AUTOHOOK_pa_alk_177_thermal_zone_checker BEGIN <<< */
+/* >>> AUTOHOOK_pa_alk_183_user_inactivity_checker BEGIN <<< */
+HOOKDEF(BOOL, WINAPI, AreFileApisANSI,
+	void
+);
 HOOKDEF(VOID, WINAPI, ExitProcess,
 	_In_ UINT uExitCode
 );
@@ -3959,5 +3962,5 @@ HOOKDEF(BOOL, WINAPI, TerminateProcess,
 	_In_ UINT uExitCode
 );
 
-/* >>> AUTOHOOK_pa_alk_177_thermal_zone_checker END <<< */
+/* >>> AUTOHOOK_pa_alk_183_user_inactivity_checker END <<< */
 
