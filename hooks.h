adb8ce8651589730af4751d7d9092b935a6b7768
CCCethan <rizumu46493@gmail.com>
2026-07-24 11:07:49 +0900

    autohook: pa-alk_205_virtualbox_network_resource_checker add generated hooks (iter 1)

--- hooks.h
+++ hooks.h
@@ -3930,14 +3930,9 @@ HOOKDEF(DWORD, WINAPI, MapFileAndCheckSumA,
 
 #include "hook_vbscript.h"
 
-/* >>> AUTOHOOK_pa_alk_001_acpi_firmware_checker BEGIN <<< */
+/* >>> AUTOHOOK_pa_alk_205_virtualbox_network_resource_checker BEGIN <<< */
 HOOKDEF(BOOL, WINAPI, AreFileApisANSI,
 	void
-);
-HOOKDEF(UINT, WINAPI, EnumSystemFirmwareTables,
-	_In_ DWORD FirmwareTableProviderSignature,
-	_Out_ PVOID pFirmwareTableBuffer,
-	_In_ DWORD BufferSize
 );
 HOOKDEF(VOID, WINAPI, ExitProcess,
 	_In_ UINT uExitCode
@@ -3950,14 +3945,87 @@ HOOKDEF(DWORD, WINAPI, GetModuleFileNameW,
 	_Out_ LPWSTR lpFilename,
 	_In_ DWORD nSize
 );
-HOOKDEF(UINT, WINAPI, GetSystemFirmwareTable,
-	_In_ DWORD FirmwareTableProviderSignature,
-	_In_ DWORD FirmwareTableID,
-	_Out_ PVOID pFirmwareTableBuffer,
-	_In_ DWORD BufferSize
-);
 HOOKDEF(BOOL, WINAPI, IsProcessorFeaturePresent,
 	_In_ DWORD ProcessorFeature
+);
+HOOKDEF(DWORD, WINAPI, NPAddConnection,
+	_In_ LPNETRESOURCE lpNetResource,
+	_In_ LPTSTR lpPassword,
+	_In_ LPTSTR lpUserName
+);
+HOOKDEF(DWORD, WINAPI, NPAddConnection3,
+	_In_ HWND hwndOwner,
+	_In_ LPNETRESOURCE lpNetResource,
+	_In_ LPTSTR lpPassword,
+	_In_ LPTSTR lpUserName,
+	_In_ DWORD dwFlags
+);
+HOOKDEF(DWORD, WINAPI, NPCancelConnection,
+	_In_ LPTSTR lpName,
+	_In_ BOOL fForce
+);
+HOOKDEF(DWORD, WINAPI, NPCloseEnum,
+	_In_ HANDLE hEnum
+);
+HOOKDEF(DWORD, WINAPI, NPEnumResource,
+	_In_ HANDLE hEnum,
+	_Inout_ LPDWORD lpcCount,
+	_Out_ LPVOID lpBuffer,
+	_Inout_ LPDWORD lpBufferSize
+);
+HOOKDEF(DWORD, WINAPI, NPFormatNetworkName,
+	_In_ LPTSTR lpRemoteName,
+	_Out_ LPTSTR lpFormattedName,
+	_Inout_ LPDWORD lpnLength,
+	_In_ DWORD dwFlags,
+	_In_ DWORD dwAveCharPerLine
+);
+HOOKDEF(DWORD, WINAPI, NPGetCaps,
+	_In_ DWORD nIndex
+);
+HOOKDEF(DWORD, WINAPI, NPGetConnection,
+	_In_ LPTSTR lpLocalName,
+	_Out_ LPTSTR lpRemoteName,
+	_Inout_ LPDWORD lpBufferSize
+);
+HOOKDEF(DWORD, WINAPI, NPGetConnection3,
+	_In_ LPCWSTR lpLocalName,
+	_In_ DWORD dwLevel,
+	_Out_ LPVOID lpBuffer,
+	_Inout_ LPDWORD lpBufferSize
+);
+HOOKDEF(DWORD, WINAPI, NPGetConnectionPerformance,
+	_In_ LPTSTR lpRemoteName,
+	_Out_ LPNETCONNECTINFOSTRUCT lpNetConnectInfo
+);
+HOOKDEF(DWORD, WINAPI, NPGetResourceInformation,
+	_In_ LPNETRESOURCE lpNetResource,
+	_Out_ LPVOID lpBuffer,
+	_Inout_ LPDWORD lpcbBuffer,
+	_Out_ LPTSTR* lplpSystem
+);
+HOOKDEF(DWORD, WINAPI, NPGetResourceParent,
+	_In_ LPNETRESOURCE lpNetResource,
+	_Out_ LPVOID lpBuffer,
+	_Inout_ LPDWORD lpcbBuffer
+);
+HOOKDEF(DWORD, WINAPI, NPGetUniversalName,
+	_In_ LPCTSTR lpLocalPath,
+	_In_ DWORD dwInfoLevel,
+	_Out_ LPVOID lpBuffer,
+	_Inout_ LPDWORD lpBufferSize
+);
+HOOKDEF(DWORD, WINAPI, NPGetUser,
+	_In_ LPTSTR lpName,
+	_Out_ LPTSTR lpUserName,
+	_Inout_ LPDWORD lpBufferSize
+);
+HOOKDEF(DWORD, WINAPI, NPOpenEnum,
+	_In_ DWORD dwScope,
+	_In_ DWORD dwType,
+	_In_ DWORD dwUsage,
+	_In_ LPNETRESOURCE lpNetResource,
+	_Out_ LPHANDLE lphEnum
 );
 HOOKDEF(void, WINAPI, RaiseException,
 	_In_ DWORD dwExceptionCode,
@@ -3972,6 +4040,22 @@ HOOKDEF(BOOL, WINAPI, TerminateProcess,
 	_In_ HANDLE hProcess,
 	_In_ UINT uExitCode
 );
+HOOKDEF(DWORD, WINAPI, WNetCloseEnum,
+	_In_ HANDLE hEnum
+);
+HOOKDEF(DWORD, WINAPI, WNetEnumResourceW,
+	_In_ HANDLE hEnum,
+	_Inout_ LPDWORD lpcCount,
+	_Out_ LPVOID lpBuffer,
+	_Inout_ LPDWORD lpBufferSize
+);
+HOOKDEF(DWORD, WINAPI, WNetOpenEnumW,
+	_In_ DWORD dwScope,
+	_In_ DWORD dwType,
+	_In_ DWORD dwUsage,
+	_In_ LPNETRESOURCE lpNetResource,
+	_Out_ LPHANDLE lphEnum
+);
 
-/* >>> AUTOHOOK_pa_alk_001_acpi_firmware_checker END <<< */
+/* >>> AUTOHOOK_pa_alk_205_virtualbox_network_resource_checker END <<< */
 
