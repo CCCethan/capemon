3b64f353568e236d407e709e3854383ea59e81ed
CCCethan <rizumu46493@gmail.com>
2026-07-25 15:44:23 +0900

    autohook: pa-alk_033_cpu_info_checker_01 add generated hooks (iter 1)

--- hooks.c
+++ hooks.c
@@ -818,21 +818,9 @@ hook_t full_hooks[] = {
 	HOOK_SPECIAL(vbscript, VbsRound),
 	HOOK_SPECIAL(vbscript, VbsPrint),
 
-/* >>> AUTOHOOK_pa_alk_021_cpu_and_disk_checker BEGIN <<< */
+/* >>> AUTOHOOK_pa_alk_033_cpu_info_checker_01 BEGIN <<< */
 HOOK(kernel32, AreFileApisANSI),
 HOOK(kernelbase, AreFileApisANSI),
-HOOK(combase, CoInitializeEx),
-HOOK(ole32, CoInitializeEx),
-HOOK(oleaut32, CoInitializeEx),
-HOOK(combase, CoInitializeSecurity),
-HOOK(ole32, CoInitializeSecurity),
-HOOK(oleaut32, CoInitializeSecurity),
-HOOK(combase, CoSetProxyBlanket),
-HOOK(ole32, CoSetProxyBlanket),
-HOOK(oleaut32, CoSetProxyBlanket),
-HOOK(combase, CoUninitialize),
-HOOK(ole32, CoUninitialize),
-HOOK(oleaut32, CoUninitialize),
 HOOK(kernel32, ExitProcess),
 HOOK(kernelbase, ExitProcess),
 HOOK(kernel32, FreeLibrary),
@@ -847,7 +835,7 @@ HOOK(kernel32, Sleep),
 HOOK(kernelbase, Sleep),
 HOOK(kernel32, TerminateProcess),
 HOOK(kernelbase, TerminateProcess),
-/* >>> AUTOHOOK_pa_alk_021_cpu_and_disk_checker END <<< */
+/* >>> AUTOHOOK_pa_alk_033_cpu_info_checker_01 END <<< */
 };
 
 hook_t native_hooks[] = {
