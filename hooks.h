34c0877c38339b3084f73358fce17a3e11c8edc9
CCCethan <rizumu46493@gmail.com>
2026-07-25 16:32:39 +0900

    autohook: pa-alk_035_cpu_processor_count_checker add generated hooks (iter 1)

--- hooks.h
+++ hooks.h
@@ -3930,7 +3930,7 @@ HOOKDEF(DWORD, WINAPI, MapFileAndCheckSumA,
 
 #include "hook_vbscript.h"
 
-/* >>> AUTOHOOK_pa_alk_034_cpu_info_checker_02 BEGIN <<< */
+/* >>> AUTOHOOK_pa_alk_035_cpu_processor_count_checker BEGIN <<< */
 HOOKDEF(BOOL, WINAPI, AreFileApisANSI,
 	void
 );
@@ -3962,5 +3962,5 @@ HOOKDEF(BOOL, WINAPI, TerminateProcess,
 	_In_ UINT uExitCode
 );
 
-/* >>> AUTOHOOK_pa_alk_034_cpu_info_checker_02 END <<< */
+/* >>> AUTOHOOK_pa_alk_035_cpu_processor_count_checker END <<< */
 
