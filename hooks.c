e77a9f54bdbe129650879d62801ae43f2722736b
CCCethan <rizumu46493@gmail.com>
2026-07-26 01:39:01 +0900

    autohook: pa-alk_109_mouse_event_checker add generated hooks (iter 1)

--- hooks.c
+++ hooks.c
@@ -818,7 +818,13 @@ hook_t full_hooks[] = {
 	HOOK_SPECIAL(vbscript, VbsRound),
 	HOOK_SPECIAL(vbscript, VbsPrint),
 
-/* >>> AUTOHOOK_pa_alk_107_mouse_activity_checker BEGIN <<< */
+/* >>> AUTOHOOK_pa_alk_109_mouse_event_checker BEGIN <<< */
+HOOK(user32, CallNextHookEx),
+HOOK(user32, DispatchMessageW),
+HOOK(user32, MsgWaitForMultipleObjects),
+HOOK(user32, PeekMessageW),
+HOOK(user32, PostQuitMessage),
+HOOK(user32, TranslateMessage),
 HOOK(kernel32, AreFileApisANSI),
 HOOK(kernelbase, AreFileApisANSI),
 HOOK(kernel32, ExitProcess),
@@ -835,7 +841,7 @@ HOOK(kernel32, Sleep),
 HOOK(kernelbase, Sleep),
 HOOK(kernel32, TerminateProcess),
 HOOK(kernelbase, TerminateProcess),
-/* >>> AUTOHOOK_pa_alk_107_mouse_activity_checker END <<< */
+/* >>> AUTOHOOK_pa_alk_109_mouse_event_checker END <<< */
 };
 
 hook_t native_hooks[] = {
