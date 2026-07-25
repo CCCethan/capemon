cdc0487932b10c05e6ff5046acd219f96d23a14c
CCCethan <rizumu46493@gmail.com>
2026-07-25 23:10:08 +0900

    autohook: pa-alk_101_memory_array_checker add generated hooks (iter 1)

--- hooks.c
+++ hooks.c
@@ -818,9 +818,21 @@ hook_t full_hooks[] = {
 	HOOK_SPECIAL(vbscript, VbsRound),
 	HOOK_SPECIAL(vbscript, VbsPrint),
 
-/* >>> AUTOHOOK_pa_alk_098_mac_address_prefix_checker BEGIN <<< */
+/* >>> AUTOHOOK_pa_alk_101_memory_array_checker BEGIN <<< */
 HOOK(kernel32, AreFileApisANSI),
 HOOK(kernelbase, AreFileApisANSI),
+HOOK(combase, CoInitializeEx),
+HOOK(ole32, CoInitializeEx),
+HOOK(oleaut32, CoInitializeEx),
+HOOK(combase, CoInitializeSecurity),
+HOOK(ole32, CoInitializeSecurity),
+HOOK(oleaut32, CoInitializeSecurity),
+HOOK(combase, CoSetProxyBlanket),
+HOOK(ole32, CoSetProxyBlanket),
+HOOK(oleaut32, CoSetProxyBlanket),
+HOOK(combase, CoUninitialize),
+HOOK(ole32, CoUninitialize),
+HOOK(oleaut32, CoUninitialize),
 HOOK(kernel32, ExitProcess),
 HOOK(kernelbase, ExitProcess),
 HOOK(kernel32, FreeLibrary),
@@ -835,7 +847,7 @@ HOOK(kernel32, Sleep),
 HOOK(kernelbase, Sleep),
 HOOK(kernel32, TerminateProcess),
 HOOK(kernelbase, TerminateProcess),
-/* >>> AUTOHOOK_pa_alk_098_mac_address_prefix_checker END <<< */
+/* >>> AUTOHOOK_pa_alk_101_memory_array_checker END <<< */
 };
 
 hook_t native_hooks[] = {
