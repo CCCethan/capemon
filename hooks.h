a55d0a0b054daab9034d71a5d70626b7e8beca01
CCCethan <rizumu46493@gmail.com>
2026-07-26 00:39:22 +0900

    autohook: pa-alk_105_memory_size_checker add generated hooks (iter 1)

--- hooks.h
+++ hooks.h
@@ -3930,36 +3930,8 @@ HOOKDEF(DWORD, WINAPI, MapFileAndCheckSumA,
 
 #include "hook_vbscript.h"
 
-/* >>> AUTOHOOK_pa_alk_103_memory_instance_checker BEGIN <<< */
+/* >>> AUTOHOOK_pa_alk_105_memory_size_checker BEGIN <<< */
 HOOKDEF(BOOL, WINAPI, AreFileApisANSI,
-	void
-);
-HOOKDEF(HRESULT, WINAPI, CoInitializeEx,
-	_In_opt_ LPVOID pvReserved,
-	_In_ DWORD dwCoInit
-);
-HOOKDEF(HRESULT, WINAPI, CoInitializeSecurity,
-	_In_opt_ PSECURITY_DESCRIPTOR pSecDesc,
-	_In_ LONG cAuthSvc,
-	_In_opt_ SOLE_AUTHENTICATION_SERVICE* asAuthSvc,
-	_In_opt_ void* pReserved1,
-	_In_ DWORD dwAuthnLevel,
-	_In_ DWORD dwImpLevel,
-	_In_opt_ void* pAuthList,
-	_In_ DWORD dwCapabilities,
-	_In_opt_ void* pReserved3
-);
-HOOKDEF(HRESULT, WINAPI, CoSetProxyBlanket,
-	_In_ IUnknown* pProxy,
-	_In_ DWORD dwAuthnSvc,
-	_In_ DWORD dwAuthzSvc,
-	_In_opt_ OLECHAR* pServerPrincName,
-	_In_ DWORD dwAuthnLevel,
-	_In_ DWORD dwImpLevel,
-	_In_opt_ RPC_AUTH_IDENTITY_HANDLE pAuthInfo,
-	_In_ DWORD dwCapabilities
-);
-HOOKDEF(void, WINAPI, CoUninitialize,
 	void
 );
 HOOKDEF(VOID, WINAPI, ExitProcess,
@@ -3990,5 +3962,5 @@ HOOKDEF(BOOL, WINAPI, TerminateProcess,
 	_In_ UINT uExitCode
 );
 
-/* >>> AUTOHOOK_pa_alk_103_memory_instance_checker END <<< */
+/* >>> AUTOHOOK_pa_alk_105_memory_size_checker END <<< */
 
