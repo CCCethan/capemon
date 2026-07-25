fbdff271aecc35b6a425a81c5dc90e9b10971043
CCCethan <rizumu46493@gmail.com>
2026-07-25 16:13:03 +0900

    autohook: pa-alk_034_cpu_info_checker_02 add generated hooks (iter 1)

--- hook_file.c
+++ hook_file.c
@@ -1927,7 +1927,7 @@ HOOKDEF(DWORD, WINAPI, RmStartSession,
 	return ret;
 }
 
-/* >>> AUTOHOOK_pa_alk_033_cpu_info_checker_01 BEGIN <<< */
+/* >>> AUTOHOOK_pa_alk_034_cpu_info_checker_02 BEGIN <<< */
 // -> hook_file.c に追加 | category="filesystem" | winapi:Files and I/O (Local file system)
 HOOKDEF(BOOL, WINAPI, AreFileApisANSI, // 呼出規約は WINAPI 仮定(socket/native/CRT系は要確認)
 	void
@@ -1937,5 +1937,5 @@ HOOKDEF(BOOL, WINAPI, AreFileApisANSI, // 呼出規約は WINAPI 仮定(socket/n
 	LOQ_bool("filesystem", "");
 	return ret;
 }
-/* >>> AUTOHOOK_pa_alk_033_cpu_info_checker_01 END <<< */
+/* >>> AUTOHOOK_pa_alk_034_cpu_info_checker_02 END <<< */
 
