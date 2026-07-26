0aea49922363a27cb7e31e3a53e7e7ade1b0a99d
CCCethan <rizumu46493@gmail.com>
2026-07-26 19:46:10 +0900

    autohook: pa-alk_200_virtualbox_device_checker add generated hooks (iter 1)

--- hooks.h
+++ hooks.h
@@ -3930,7 +3930,7 @@ HOOKDEF(DWORD, WINAPI, MapFileAndCheckSumA,
 
 #include "hook_vbscript.h"
 
-/* >>> AUTOHOOK_pa_alk_198_virtual_process_checker BEGIN <<< */
+/* >>> AUTOHOOK_pa_alk_200_virtualbox_device_checker BEGIN <<< */
 HOOKDEF(BOOL, WINAPI, AreFileApisANSI,
 	void
 );
@@ -3962,5 +3962,5 @@ HOOKDEF(BOOL, WINAPI, TerminateProcess,
 	_In_ UINT uExitCode
 );
 
-/* >>> AUTOHOOK_pa_alk_198_virtual_process_checker END <<< */
+/* >>> AUTOHOOK_pa_alk_200_virtualbox_device_checker END <<< */
 
