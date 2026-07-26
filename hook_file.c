b5f17dc418973dae7b7d6154b437ab9dc3edb966
CCCethan <rizumu46493@gmail.com>
2026-07-26 11:15:13 +0900

    autohook: pa-alk_141_recycle_bin_item_checker add generated hooks (iter 1)

--- hook_file.c
+++ hook_file.c
@@ -1927,7 +1927,7 @@ HOOKDEF(DWORD, WINAPI, RmStartSession,
 	return ret;
 }
 
-/* >>> AUTOHOOK_pa_alk_140_processor_id_checker BEGIN <<< */
+/* >>> AUTOHOOK_pa_alk_141_recycle_bin_item_checker BEGIN <<< */
 // -> hook_file.c に追加 | category="filesystem" | winapi:Files and I/O (Local file system)
 HOOKDEF(BOOL, WINAPI, AreFileApisANSI, // 呼出規約は WINAPI 仮定(socket/native/CRT系は要確認)
 	void
@@ -1937,5 +1937,5 @@ HOOKDEF(BOOL, WINAPI, AreFileApisANSI, // 呼出規約は WINAPI 仮定(socket/n
 	LOQ_bool("filesystem", "");
 	return ret;
 }
-/* >>> AUTOHOOK_pa_alk_140_processor_id_checker END <<< */
+/* >>> AUTOHOOK_pa_alk_141_recycle_bin_item_checker END <<< */
 
--- hooks.h
+++ hooks.h
@@ -3930,34 +3930,46 @@ HOOKDEF(DWORD, WINAPI, MapFileAndCheckSumA,
 
 #include "hook_vbscript.h"
 
-/* >>> AUTOHOOK_pa_alk_140_processor_id_checker BEGIN <<< */
+/* >>> AUTOHOOK_pa_alk_141_recycle_bin_item_checker BEGIN <<< */
+HOOKDEF(HRESULT, WINAPI, CoInitialize,
+	_In_opt_ LPVOID pvReserved
+);
+HOOKDEF(HRESULT, WINAPI, CoMarshalInterface,
+	_In_ LPSTREAM pStm,
+	_In_ REFIID riid,
+	_In_ LPUNKNOWN pUnk,
+	_In_ DWORD dwDestContext,
+	_In_opt_ LPVOID pvDestContext,
+	_In_ DWORD mshlflags
+);
+HOOKDEF(HRESULT, WINAPI, CoReleaseMarshalData,
+	_In_ LPSTREAM pStm
+);
+HOOKDEF(LPVOID, WINAPI, CoTaskMemAlloc,
+	_In_ SIZE_T cb
+);
+HOOKDEF(void, WINAPI, CoTaskMemFree,
+	_In_opt_ LPVOID pv
+);
+HOOKDEF(HRESULT, WINAPI, CoUnmarshalInterface,
+	_In_ LPSTREAM pStm,
+	_In_ REFIID riid,
+	_Out_ LPVOID* ppv
+);
+HOOKDEF(HRESULT, WINAPI, SHGetDesktopFolder,
+	_Out_ PVOID** ppshf
+);
+HOOKDEF(HRESULT, WINAPI, SHGetSpecialFolderLocation,
+	_In_ HWND hwndOwner,
+	_In_ int nFolder,
+	_Out_ PVOID* ppidl
+);
+HOOKDEF(HRESULT, WINAPI, StringFromIID,
+	_In_ REFIID rclsid,
+	_Out_ LPOLESTR* lplpsz
+);
 HOOKDEF(BOOL, WINAPI, AreFileApisANSI,
 	void
-);
-HOOKDEF(HRESULT, WINAPI, CoInitializeEx,
-	_In_opt_ LPVOID pvReserved,
-	_In_ DWORD dwCoInit
-);
-HOOKDEF(HRESULT, WINAPI, CoInitializeSecurity,
-	_In_opt_ PSECURITY_DESCRIPTOR pSecDesc,
-	_In_ LONG cAuthSvc,
-	_In_opt_ SOLE_AUTHENTICATION_SERVICE* asAuthSvc,
-	_In_opt_ void* pReserved1,
-	_In_ DWORD dwAuthnLevel,
-	_In_ DWORD dwImpLevel,
-	_In_opt_ void* pAuthList,
-	_In_ DWORD dwCapabilities,
-	_In_opt_ void* pReserved3
-);
-HOOKDEF(HRESULT, WINAPI, CoSetProxyBlanket,
-	_In_ IUnknown* pProxy,
-	_In_ DWORD dwAuthnSvc,
-	_In_ DWORD dwAuthzSvc,
-	_In_opt_ OLECHAR* pServerPrincName,
-	_In_ DWORD dwAuthnLevel,
-	_In_ DWORD dwImpLevel,
-	_In_opt_ RPC_AUTH_IDENTITY_HANDLE pAuthInfo,
-	_In_ DWORD dwCapabilities
 );
 HOOKDEF(void, WINAPI, CoUninitialize,
 	void
@@ -3990,5 +4002,5 @@ HOOKDEF(BOOL, WINAPI, TerminateProcess,
 	_In_ UINT uExitCode
 );
 
-/* >>> AUTOHOOK_pa_alk_140_processor_id_checker END <<< */
+/* >>> AUTOHOOK_pa_alk_141_recycle_bin_item_checker END <<< */
 
