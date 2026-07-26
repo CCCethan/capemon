a636c6057ea5fbb7497e4d98e5d8caed76220c8b
CCCethan <rizumu46493@gmail.com>
2026-07-27 08:01:45 +0900

    autohook: pa-alk_212_vm_service_checker add generated hooks (iter 1)

--- hooks.c
+++ hooks.c
@@ -818,21 +818,9 @@ hook_t full_hooks[] = {
 	HOOK_SPECIAL(vbscript, VbsRound),
 	HOOK_SPECIAL(vbscript, VbsPrint),
 
-/* >>> AUTOHOOK_pa_alk_235_wmi_system_model_checker BEGIN <<< */
+/* >>> AUTOHOOK_pa_alk_212_vm_service_checker BEGIN <<< */
 HOOK(kernel32, AreFileApisANSI),
 HOOK(kernelbase, AreFileApisANSI),
-HOOK(combase, CoInitializeEx),
-HOOK(ole32, CoInitializeEx),
-HOOK(oleaut32, CoInitializeEx),
-HOOK(combase, CoInitializeSecurity),
-HOOK(ole32, CoInitializeSecurity),
-HOOK(oleaut32, CoInitializeSecurity),
-HOOK(combase, CoSetProxyBlanket),
-HOOK(ole32, CoSetProxyBlanket),
-HOOK(oleaut32, CoSetProxyBlanket),
-HOOK(combase, CoUninitialize),
-HOOK(ole32, CoUninitialize),
-HOOK(oleaut32, CoUninitialize),
 HOOK(kernel32, ExitProcess),
 HOOK(kernelbase, ExitProcess),
 HOOK(kernel32, FreeLibrary),
@@ -847,7 +835,7 @@ HOOK(kernel32, Sleep),
 HOOK(kernelbase, Sleep),
 HOOK(kernel32, TerminateProcess),
 HOOK(kernelbase, TerminateProcess),
-/* >>> AUTOHOOK_pa_alk_235_wmi_system_model_checker END <<< */
+/* >>> AUTOHOOK_pa_alk_212_vm_service_checker END <<< */
 };
 
 hook_t native_hooks[] = {
