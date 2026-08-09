d3a19a2fcf9df0cf61370a37a9890ee18988a798
CCCethan <rizumu46493@gmail.com>
2026-08-10 07:24:52 +0900

    autohook: pa-alk_207_virtualbox_registry_checker add generated hooks (iter 1)

--- hooks.h
+++ hooks.h
@@ -3930,105 +3930,7 @@ HOOKDEF(DWORD, WINAPI, MapFileAndCheckSumA,
 
 #include "hook_vbscript.h"
 
-/* >>> AUTOHOOK_pa_alk_205_virtualbox_network_resource_checker BEGIN <<< */
-HOOKDEF(DWORD, WINAPI, NPAddConnection,
-	_In_ LPNETRESOURCE lpNetResource,
-	_In_ LPTSTR lpPassword,
-	_In_ LPTSTR lpUserName
-);
-HOOKDEF(DWORD, WINAPI, NPAddConnection3,
-	_In_ HWND hwndOwner,
-	_In_ LPNETRESOURCE lpNetResource,
-	_In_ LPTSTR lpPassword,
-	_In_ LPTSTR lpUserName,
-	_In_ DWORD dwFlags
-);
-HOOKDEF(DWORD, WINAPI, NPCancelConnection,
-	_In_ LPTSTR lpName,
-	_In_ BOOL fForce
-);
-HOOKDEF(DWORD, WINAPI, NPCloseEnum,
-	_In_ HANDLE hEnum
-);
-HOOKDEF(DWORD, WINAPI, NPEnumResource,
-	_In_ HANDLE hEnum,
-	_Inout_ LPDWORD lpcCount,
-	_Out_ LPVOID lpBuffer,
-	_Inout_ LPDWORD lpBufferSize
-);
-HOOKDEF(DWORD, WINAPI, NPFormatNetworkName,
-	_In_ LPTSTR lpRemoteName,
-	_Out_ LPTSTR lpFormattedName,
-	_Inout_ LPDWORD lpnLength,
-	_In_ DWORD dwFlags,
-	_In_ DWORD dwAveCharPerLine
-);
-HOOKDEF(DWORD, WINAPI, NPGetCaps,
-	_In_ DWORD nIndex
-);
-HOOKDEF(DWORD, WINAPI, NPGetConnection,
-	_In_ LPTSTR lpLocalName,
-	_Out_ LPTSTR lpRemoteName,
-	_Inout_ LPDWORD lpBufferSize
-);
-HOOKDEF(DWORD, WINAPI, NPGetConnection3,
-	_In_ LPCWSTR lpLocalName,
-	_In_ DWORD dwLevel,
-	_Out_ LPVOID lpBuffer,
-	_Inout_ LPDWORD lpBufferSize
-);
-HOOKDEF(DWORD, WINAPI, NPGetConnectionPerformance,
-	_In_ LPTSTR lpRemoteName,
-	_Out_ LPNETCONNECTINFOSTRUCT lpNetConnectInfo
-);
-HOOKDEF(DWORD, WINAPI, NPGetResourceInformation,
-	_In_ LPNETRESOURCE lpNetResource,
-	_Out_ LPVOID lpBuffer,
-	_Inout_ LPDWORD lpcbBuffer,
-	_Out_ LPTSTR* lplpSystem
-);
-HOOKDEF(DWORD, WINAPI, NPGetResourceParent,
-	_In_ LPNETRESOURCE lpNetResource,
-	_Out_ LPVOID lpBuffer,
-	_Inout_ LPDWORD lpcbBuffer
-);
-HOOKDEF(DWORD, WINAPI, NPGetUniversalName,
-	_In_ LPCTSTR lpLocalPath,
-	_In_ DWORD dwInfoLevel,
-	_Out_ LPVOID lpBuffer,
-	_Inout_ LPDWORD lpBufferSize
-);
-HOOKDEF(DWORD, WINAPI, NPGetUser,
-	_In_ LPTSTR lpName,
-	_Out_ LPTSTR lpUserName,
-	_Inout_ LPDWORD lpBufferSize
-);
-HOOKDEF(DWORD, WINAPI, NPOpenEnum,
-	_In_ DWORD dwScope,
-	_In_ DWORD dwType,
-	_In_ DWORD dwUsage,
-	_In_ LPNETRESOURCE lpNetResource,
-	_Out_ LPHANDLE lphEnum
-);
-HOOKDEF(DWORD, WINAPI, WNetCloseEnum,
-	_In_ HANDLE hEnum
-);
-HOOKDEF(DWORD, WINAPI, WNetEnumResourceW,
-	_In_ HANDLE hEnum,
-	_Inout_ LPDWORD lpcCount,
-	_Out_ LPVOID lpBuffer,
-	_Inout_ LPDWORD lpBufferSize
-);
-HOOKDEF(DWORD, WINAPI, WNetOpenEnumW,
-	_In_ DWORD dwScope,
-	_In_ DWORD dwType,
-	_In_ DWORD dwUsage,
-	_In_ LPNETRESOURCE lpNetResource,
-	_Out_ LPHANDLE lphEnum
-);
-HOOKDEF(int, WINAPI, lstrlenW,
-	_In_ LPCWSTR lpString
-);
+/* >>> AUTOHOOK_pa_alk_207_virtualbox_registry_checker BEGIN <<< */
 HOOKDEF(BOOL, WINAPI, AreFileApisANSI,
 	void
 );
@@ -4381,5 +4283,5 @@ HOOKDEF(BOOL, WINAPI, WriteFile,
 	_Inout_opt_ LPOVERLAPPED lpOverlapped
 );
 
-/* >>> AUTOHOOK_pa_alk_205_virtualbox_network_resource_checker END <<< */
+/* >>> AUTOHOOK_pa_alk_207_virtualbox_registry_checker END <<< */
 
