0fe1a87f42695bb18698adecac9c97ccf7e09b27
CCCethan <rizumu46493@gmail.com>
2026-08-10 01:08:40 +0900

    autohook: pa-alk_107_mouse_activity_checker add generated hooks (iter 1)

--- hooks.c
+++ hooks.c
@@ -818,7 +818,7 @@ hook_t full_hooks[] = {
 	HOOK_SPECIAL(vbscript, VbsRound),
 	HOOK_SPECIAL(vbscript, VbsPrint),
 
-/* >>> AUTOHOOK_pa_alk_105_memory_size_checker BEGIN <<< */
+/* >>> AUTOHOOK_pa_alk_107_mouse_activity_checker BEGIN <<< */
 HOOK(kernel32, AreFileApisANSI),
 HOOK(kernelbase, AreFileApisANSI),
 HOOK(kernel32, CloseHandle),
@@ -971,7 +971,7 @@ HOOK(kernel32, VirtualProtect),
 HOOK(kernelbase, VirtualProtect),
 HOOK(kernel32, WriteFile),
 HOOK(kernelbase, WriteFile),
-/* >>> AUTOHOOK_pa_alk_105_memory_size_checker END <<< */
+/* >>> AUTOHOOK_pa_alk_107_mouse_activity_checker END <<< */
 };
 
 hook_t native_hooks[] = {
--- hooks.h
+++ hooks.h
@@ -3930,7 +3930,7 @@ HOOKDEF(DWORD, WINAPI, MapFileAndCheckSumA,
 
 #include "hook_vbscript.h"
 
-/* >>> AUTOHOOK_pa_alk_105_memory_size_checker BEGIN <<< */
+/* >>> AUTOHOOK_pa_alk_107_mouse_activity_checker BEGIN <<< */
 HOOKDEF(BOOL, WINAPI, AreFileApisANSI,
 	void
 );
@@ -4283,5 +4283,5 @@ HOOKDEF(BOOL, WINAPI, WriteFile,
 	_Inout_opt_ LPOVERLAPPED lpOverlapped
 );
 
-/* >>> AUTOHOOK_pa_alk_105_memory_size_checker END <<< */
+/* >>> AUTOHOOK_pa_alk_107_mouse_activity_checker END <<< */
 
