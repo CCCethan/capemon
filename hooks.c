d51a19cad35079a91959ed0b4c3000b0298261d3
CCCethan <rizumu46493@gmail.com>
2026-08-10 07:55:42 +0900

    autohook: pa-alk_212_vm_service_checker add generated hooks (iter 1)

--- hooks.c
+++ hooks.c
@@ -818,7 +818,8 @@ hook_t full_hooks[] = {
 	HOOK_SPECIAL(vbscript, VbsRound),
 	HOOK_SPECIAL(vbscript, VbsPrint),
 
-/* >>> AUTOHOOK_pa_alk_209_virtualbox_window_checker BEGIN <<< */
+/* >>> AUTOHOOK_pa_alk_212_vm_service_checker BEGIN <<< */
+HOOK(advapi32, CloseServiceHandle),
 HOOK(kernel32, AreFileApisANSI),
 HOOK(kernelbase, AreFileApisANSI),
 HOOK(kernel32, CloseHandle),
@@ -971,7 +972,7 @@ HOOK(kernel32, VirtualProtect),
 HOOK(kernelbase, VirtualProtect),
 HOOK(kernel32, WriteFile),
 HOOK(kernelbase, WriteFile),
-/* >>> AUTOHOOK_pa_alk_209_virtualbox_window_checker END <<< */
+/* >>> AUTOHOOK_pa_alk_212_vm_service_checker END <<< */
 };
 
 hook_t native_hooks[] = {
