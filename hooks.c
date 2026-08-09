8803f239d8ba2df7984cda1de1b21924e501f624
CCCethan <rizumu46493@gmail.com>
2026-08-10 00:00:25 +0900

    autohook: pa-alk_093_mac_address_checker add generated hooks (iter 1)

--- hooks.c
+++ hooks.c
@@ -818,7 +818,7 @@ hook_t full_hooks[] = {
 	HOOK_SPECIAL(vbscript, VbsRound),
 	HOOK_SPECIAL(vbscript, VbsPrint),
 
-/* >>> AUTOHOOK_pa_alk_084_hypervisor_checker BEGIN <<< */
+/* >>> AUTOHOOK_pa_alk_093_mac_address_checker BEGIN <<< */
 HOOK(kernel32, AreFileApisANSI),
 HOOK(kernelbase, AreFileApisANSI),
 HOOK(kernel32, CloseHandle),
@@ -971,7 +971,7 @@ HOOK(kernel32, VirtualProtect),
 HOOK(kernelbase, VirtualProtect),
 HOOK(kernel32, WriteFile),
 HOOK(kernelbase, WriteFile),
-/* >>> AUTOHOOK_pa_alk_084_hypervisor_checker END <<< */
+/* >>> AUTOHOOK_pa_alk_093_mac_address_checker END <<< */
 };
 
 hook_t native_hooks[] = {
--- hooks.h
+++ hooks.h
@@ -3930,7 +3930,7 @@ HOOKDEF(DWORD, WINAPI, MapFileAndCheckSumA,
 
 #include "hook_vbscript.h"
 
-/* >>> AUTOHOOK_pa_alk_084_hypervisor_checker BEGIN <<< */
+/* >>> AUTOHOOK_pa_alk_093_mac_address_checker BEGIN <<< */
 HOOKDEF(BOOL, WINAPI, AreFileApisANSI,
 	void
 );
@@ -4283,5 +4283,5 @@ HOOKDEF(BOOL, WINAPI, WriteFile,
 	_Inout_opt_ LPOVERLAPPED lpOverlapped
 );
 
-/* >>> AUTOHOOK_pa_alk_084_hypervisor_checker END <<< */
+/* >>> AUTOHOOK_pa_alk_093_mac_address_checker END <<< */
 
