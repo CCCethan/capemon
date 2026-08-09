4b0c560b0e45ab6dbce1f19fc6e3037731d5a236
CCCethan <rizumu46493@gmail.com>
2026-08-10 02:43:17 +0900

    autohook: pa-alk_132_printer_presence_checker add generated hooks (iter 1)

--- hooks.h
+++ hooks.h
@@ -3930,51 +3930,18 @@ HOOKDEF(DWORD, WINAPI, MapFileAndCheckSumA,
 
 #include "hook_vbscript.h"
 
-/* >>> AUTOHOOK_pa_alk_020_computer_system_manufacturer_checker BEGIN <<< */
-HOOKDEF(BOOL, WINAPI, AreFileApisANSI,
-	void
+/* >>> AUTOHOOK_pa_alk_132_printer_presence_checker BEGIN <<< */
+HOOKDEF(BOOL, WINAPI, EnumPrintersA,
+	_In_ DWORD Flags,
+	_In_ LPSTR Name,
+	_In_ DWORD Level,
+	_Out_ LPBYTE pPrinterEnum,
+	_In_ DWORD cbBuf,
+	_Out_ LPDWORD pcbNeeded,
+	_Out_ LPDWORD pcReturned
 );
 HOOKDEF(BOOL, WINAPI, CloseHandle,
 	_In_ HANDLE hObject
-);
-HOOKDEF(HRESULT, WINAPI, CoInitializeEx,
-	_In_opt_ LPVOID pvReserved,
-	_In_ DWORD dwCoInit
-);
-HOOKDEF(HRESULT, WINAPI, CoInitializeSecurity,
-	_In_opt_ PSECURITY_DESCRIPTOR pSecDesc,
-	_In_ LONG cAuthSvc,
-	_In_opt_ SOLE_AUTHENTICATION_SERVICE* asAuthSvc,
-	_In_opt_ void* pReserved1,
-	_In_ DWORD dwAuthnLevel,
-	_In_ DWORD dwImpLevel,
-	_In_opt_ void* pAuthList,
-	_In_ DWORD dwCapabilities,
-	_In_opt_ void* pReserved3
-);
-HOOKDEF(HRESULT, WINAPI, CoSetProxyBlanket,
-	_In_ IUnknown* pProxy,
-	_In_ DWORD dwAuthnSvc,
-	_In_ DWORD dwAuthzSvc,
-	_In_opt_ OLECHAR* pServerPrincName,
-	_In_ DWORD dwAuthnLevel,
-	_In_ DWORD dwImpLevel,
-	_In_opt_ RPC_AUTH_IDENTITY_HANDLE pAuthInfo,
-	_In_ DWORD dwCapabilities
-);
-HOOKDEF(void, WINAPI, CoUninitialize,
-	void
-);
-HOOKDEF(int, WINAPI, CompareStringEx,
-	_In_opt_ LPCWSTR lpLocaleName,
-	_In_ DWORD dwCmpFlags,
-	_In_ LPCWSTR lpString1,
-	_In_ int cchCount1,
-	_In_ LPCWSTR lpString2,
-	_In_ int cchCount2,
-	_In_opt_ LPNLSVERSIONINFO lpVersionInformation,
-	_In_opt_ LPVOID lpReserved,
-	_In_opt_ LPARAM lParam
 );
 HOOKDEF(int, WINAPI, CompareStringW,
 	_In_ LCID Locale,
@@ -3998,12 +3965,6 @@ HOOKDEF(void, WINAPI, DeleteCriticalSection,
 );
 HOOKDEF(void, WINAPI, EnterCriticalSection,
 	_Inout_ LPCRITICAL_SECTION lpCriticalSection
-);
-HOOKDEF(BOOL, WINAPI, EnumSystemLocalesEx,
-	_In_ LOCALE_ENUMPROCEX lpLocaleEnumProcEx,
-	_In_ DWORD dwFlags,
-	_In_ LPARAM lParam,
-	_In_opt_ LPVOID lpReserved
 );
 HOOKDEF(BOOL, WINAPI, EnumSystemLocalesW,
 	_In_ LOCALE_ENUMPROC lpLocaleEnumProc,
@@ -4047,15 +4008,6 @@ HOOKDEF(DWORD, WINAPI, GetCurrentProcessId,
 HOOKDEF(DWORD, WINAPI, GetCurrentThreadId,
 	void
 );
-HOOKDEF(int, WINAPI, GetDateFormatEx,
-	_In_opt_ LPCWSTR lpLocaleName,
-	_In_ DWORD dwFlags,
-	_In_opt_ const SYSTEMTIME* lpDate,
-	_In_opt_ LPCWSTR lpFormat,
-	_Out_opt_ LPWSTR lpDateStr,
-	_In_ int cchDate,
-	_In_opt_ LPCWSTR lpCalendar
-);
 HOOKDEF(int, WINAPI, GetDateFormatW,
 	_In_ LCID Locale,
 	_In_ DWORD dwFlags,
@@ -4073,12 +4025,6 @@ HOOKDEF(BOOL, WINAPI, GetFileSizeEx,
 );
 HOOKDEF(DWORD, WINAPI, GetFileType,
 	_In_ HANDLE hFile
-);
-HOOKDEF(int, WINAPI, GetLocaleInfoEx,
-	_In_opt_ LPCWSTR lpLocaleName,
-	_In_ LCTYPE LCType,
-	_Out_opt_ LPWSTR lpLCData,
-	_In_ int cchData
 );
 HOOKDEF(int, WINAPI, GetLocaleInfoW,
 	_In_ LCID Locale,
@@ -4121,14 +4067,6 @@ HOOKDEF(BOOL, WINAPI, GetStringTypeW,
 	_In_ int cchSrc,
 	_Out_ LPWORD lpCharType
 );
-HOOKDEF(int, WINAPI, GetTimeFormatEx,
-	_In_opt_ LPCWSTR lpLocaleName,
-	_In_ DWORD dwFlags,
-	_In_opt_ const SYSTEMTIME* lpTime,
-	_In_opt_ LPCWSTR lpFormat,
-	_Out_opt_ LPWSTR lpTimeStr,
-	_In_ int cchTime
-);
 HOOKDEF(int, WINAPI, GetTimeFormatW,
 	_In_ LCID Locale,
 	_In_ DWORD dwFlags,
@@ -4139,10 +4077,6 @@ HOOKDEF(int, WINAPI, GetTimeFormatW,
 );
 HOOKDEF(DWORD, WINAPI, GetTimeZoneInformation,
 	_Out_ LPTIME_ZONE_INFORMATION lpTimeZoneInformation
-);
-HOOKDEF(int, WINAPI, GetUserDefaultLocaleName,
-	_Out_ LPWSTR lpLocaleName,
-	_In_ int cchLocaleName
 );
 HOOKDEF(LPVOID, WINAPI, HeapAlloc,
 	_In_ HANDLE hHeap,
@@ -4184,15 +4118,6 @@ HOOKDEF(BOOL, WINAPI, IsValidLocale,
 	_In_ LCID Locale,
 	_In_ DWORD dwFlags
 );
-HOOKDEF(BOOL, WINAPI, IsValidLocaleName,
-	_In_ LPCWSTR lpLocaleName
-);
-HOOKDEF(int, WINAPI, LCIDToLocaleName,
-	_In_ LCID Locale,
-	_Out_opt_ LPWSTR lpName,
-	_In_ int cchName,
-	_In_ DWORD dwFlags
-);
 HOOKDEF(int, WINAPI, LCMapStringEx,
 	_In_opt_ LPCWSTR lpLocaleName,
 	_In_ DWORD dwMapFlags,
@@ -4214,10 +4139,6 @@ HOOKDEF(int, WINAPI, LCMapStringW,
 );
 HOOKDEF(void, WINAPI, LeaveCriticalSection,
 	_Inout_ LPCRITICAL_SECTION lpCriticalSection
-);
-HOOKDEF(LCID, WINAPI, LocaleNameToLCID,
-	_In_ LPCWSTR lpName,
-	_In_ DWORD dwFlags
 );
 HOOKDEF(BOOL, WINAPI, QueryPerformanceCounter,
 	_Out_ LARGE_INTEGER* lpPerformanceCount
@@ -4280,9 +4201,6 @@ HOOKDEF(BOOL, WINAPI, SetStdHandle,
 HOOKDEF(VOID, WINAPI, Sleep,
 	_In_ DWORD dwMilliseconds
 );
-HOOKDEF(BSTR, WINAPI, SysAllocString,
-	_In_opt_ const OLECHAR* psz
-);
 HOOKDEF(BOOL, WINAPI, TerminateProcess,
 	_In_ HANDLE hProcess,
 	_In_ UINT uExitCode
@@ -4300,9 +4218,6 @@ HOOKDEF(BOOL, WINAPI, TlsSetValue,
 	_In_ DWORD dwTlsIndex,
 	_In_opt_ LPVOID lpTlsValue
 );
-HOOKDEF(HRESULT, WINAPI, VariantClear,
-	_Inout_ VARIANTARG* pvarg
-);
 HOOKDEF(BOOL, WINAPI, VirtualProtect,
 	_In_ LPVOID lpAddress,
 	_In_ SIZE_T dwSize,
@@ -4317,5 +4232,5 @@ HOOKDEF(BOOL, WINAPI, WriteFile,
 	_Inout_opt_ LPOVERLAPPED lpOverlapped
 );
 
-/* >>> AUTOHOOK_pa_alk_020_computer_system_manufacturer_checker END <<< */
+/* >>> AUTOHOOK_pa_alk_132_printer_presence_checker END <<< */
 
