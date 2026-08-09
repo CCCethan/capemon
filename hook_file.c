e7ce5adf584024ab5a779b26d42a85ea7a08592b
CCCethan <rizumu46493@gmail.com>
2026-08-10 03:12:20 +0900

    autohook: pa-alk_140_processor_id_checker add generated hooks (iter 1)

--- hook_file.c
+++ hook_file.c
@@ -1929,7 +1929,7 @@ HOOKDEF(DWORD, WINAPI, RmStartSession,
 	return ret;
 }
 
-/* >>> AUTOHOOK_pa_alk_133_process_based_env_checker BEGIN <<< */
+/* >>> AUTOHOOK_pa_alk_140_processor_id_checker BEGIN <<< */
 // -> hook_file.c に追加 | category="filesystem" | winapi:Files and I/O (Local file system)
 HOOKDEF(BOOL, WINAPI, AreFileApisANSI, // 呼出規約は WINAPI 仮定(socket/native/CRT系は要確認)
 	void
@@ -2057,5 +2057,5 @@ HOOKDEF(BOOL, WINAPI, WriteFile, // 呼出規約は WINAPI 仮定(socket/native/
 	LOQ_bool("filesystem", "pbiIP", "File", hFile, "Buffer", (size_t)nNumberOfBytesToWrite, lpBuffer, "NumberOfBytesToWrite", nNumberOfBytesToWrite, "NumberOfBytesWritten", lpNumberOfBytesWritten, "Overlapped", lpOverlapped);
 	return ret;
 }
-/* >>> AUTOHOOK_pa_alk_133_process_based_env_checker END <<< */
+/* >>> AUTOHOOK_pa_alk_140_processor_id_checker END <<< */
 
