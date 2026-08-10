c7e711ed852dcac2aa2ea91ec61451aa142815a5
CCCethan <rizumu46493@gmail.com>
2026-08-11 06:01:20 +0900

    autohook: galloro_023_cpuid_hypervisor_vendor_checker add generated hooks (iter 1)

--- hooks.h
+++ hooks.h
@@ -3930,7 +3930,7 @@ HOOKDEF(DWORD, WINAPI, MapFileAndCheckSumA,
 
 #include "hook_vbscript.h"
 
-/* >>> AUTOHOOK_galloro_022_installed_apps_registry_checker BEGIN <<< */
+/* >>> AUTOHOOK_galloro_023_cpuid_hypervisor_vendor_checker BEGIN <<< */
 HOOKDEF(BOOL, WINAPI, AreFileApisANSI,
 	void
 );
@@ -4283,5 +4283,5 @@ HOOKDEF(BOOL, WINAPI, WriteFile,
 	_Inout_opt_ LPOVERLAPPED lpOverlapped
 );
 
-/* >>> AUTOHOOK_galloro_022_installed_apps_registry_checker END <<< */
+/* >>> AUTOHOOK_galloro_023_cpuid_hypervisor_vendor_checker END <<< */
 
