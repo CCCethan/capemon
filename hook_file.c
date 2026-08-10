52cf1e9ccd48846dfead1e00292142738b32e87d
CCCethan <rizumu46493@gmail.com>
2026-08-11 01:54:40 +0900

    autohook: galloro_003_battery_status_variability_checker add generated hooks (iter 1)

--- hook_file.c
+++ hook_file.c
@@ -1929,7 +1929,7 @@ HOOKDEF(DWORD, WINAPI, RmStartSession,
 	return ret;
 }
 
-/* >>> AUTOHOOK_galloro_002_antivirus_product_checker BEGIN <<< */
+/* >>> AUTOHOOK_galloro_003_battery_status_variability_checker BEGIN <<< */
 // -> hook_file.c に追加 | category="filesystem" | winapi:Files and I/O (Local file system)
 HOOKDEF(BOOL, WINAPI, AreFileApisANSI, // 呼出規約は WINAPI 仮定(socket/native/CRT系は要確認)
 	void
@@ -2060,5 +2060,5 @@ HOOKDEF(BOOL, WINAPI, WriteFile, // 呼出規約は WINAPI 仮定(socket/native/
 	LOQ_bool("filesystem", "pbiIP", "File", hFile, "Buffer", (size_t)nNumberOfBytesToWrite, lpBuffer, "NumberOfBytesToWrite", nNumberOfBytesToWrite, "NumberOfBytesWritten", lpNumberOfBytesWritten, "Overlapped", lpOverlapped);
 	return ret;
 }
-/* >>> AUTOHOOK_galloro_002_antivirus_product_checker END <<< */
+/* >>> AUTOHOOK_galloro_003_battery_status_variability_checker END <<< */
 
