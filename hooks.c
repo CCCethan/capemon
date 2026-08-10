f1f771156b7056324e67728dae3008b8e15735dc
CCCethan <rizumu46493@gmail.com>
2026-08-11 07:27:24 +0900

    autohook: galloro_032_mouse_click_detector add generated hooks (iter 1)

--- hooks.c
+++ hooks.c
@@ -818,7 +818,7 @@ hook_t full_hooks[] = {
 	HOOK_SPECIAL(vbscript, VbsRound),
 	HOOK_SPECIAL(vbscript, VbsPrint),
 
-/* >>> AUTOHOOK_galloro_031_mouse_activity_checker BEGIN <<< */
+/* >>> AUTOHOOK_galloro_032_mouse_click_detector BEGIN <<< */
 HOOK(kernel32, AreFileApisANSI),
 HOOK(kernelbase, AreFileApisANSI),
 HOOK(kernel32, CloseHandle),
@@ -971,7 +971,7 @@ HOOK(kernel32, VirtualProtect),
 HOOK(kernelbase, VirtualProtect),
 HOOK(kernel32, WriteFile),
 HOOK(kernelbase, WriteFile),
-/* >>> AUTOHOOK_galloro_031_mouse_activity_checker END <<< */
+/* >>> AUTOHOOK_galloro_032_mouse_click_detector END <<< */
 };
 
 hook_t native_hooks[] = {
