5720b4ccd588c9c07362811571d817bdb47ea6bd
CCCethan <rizumu46493@gmail.com>
2026-08-11 06:39:46 +0900

    autohook: galloro_027_location_registry_checker add generated hooks (iter 1)

--- hooks.c
+++ hooks.c
@@ -818,14 +818,11 @@ hook_t full_hooks[] = {
 	HOOK_SPECIAL(vbscript, VbsRound),
 	HOOK_SPECIAL(vbscript, VbsPrint),
 
-/* >>> AUTOHOOK_galloro_026_large_file_downloads_checker BEGIN <<< */
+/* >>> AUTOHOOK_galloro_027_location_registry_checker BEGIN <<< */
 HOOK(kernel32, AreFileApisANSI),
 HOOK(kernelbase, AreFileApisANSI),
 HOOK(kernel32, CloseHandle),
 HOOK(kernelbase, CloseHandle),
-HOOK(combase, CoTaskMemFree),
-HOOK(ole32, CoTaskMemFree),
-HOOK(oleaut32, CoTaskMemFree),
 HOOK(kernel32, CompareStringEx),
 HOOK(kernelbase, CompareStringEx),
 HOOK(kernel32, CompareStringW),
@@ -844,12 +841,8 @@ HOOK(kernel32, ExitProcess),
 HOOK(kernelbase, ExitProcess),
 HOOK(kernel32, FindClose),
 HOOK(kernelbase, FindClose),
-HOOK(kernel32, FindFirstFileW),
-HOOK(kernelbase, FindFirstFileW),
 HOOK(kernel32, FlushFileBuffers),
 HOOK(kernelbase, FlushFileBuffers),
-HOOK(kernel32, FormatMessageA),
-HOOK(kernelbase, FormatMessageA),
 HOOK(kernel32, FreeEnvironmentStringsW),
 HOOK(kernelbase, FreeEnvironmentStringsW),
 HOOK(kernel32, FreeLibrary),
@@ -874,10 +867,6 @@ HOOK(kernel32, GetDateFormatW),
 HOOK(kernelbase, GetDateFormatW),
 HOOK(kernel32, GetEnvironmentStringsW),
 HOOK(kernelbase, GetEnvironmentStringsW),
-HOOK(kernel32, GetFileAttributesExW),
-HOOK(kernelbase, GetFileAttributesExW),
-HOOK(kernel32, GetFileInformationByHandleEx),
-HOOK(kernelbase, GetFileInformationByHandleEx),
 HOOK(kernel32, GetFileSizeEx),
 HOOK(kernelbase, GetFileSizeEx),
 HOOK(kernel32, GetFileType),
@@ -982,7 +971,7 @@ HOOK(kernel32, VirtualProtect),
 HOOK(kernelbase, VirtualProtect),
 HOOK(kernel32, WriteFile),
 HOOK(kernelbase, WriteFile),
-/* >>> AUTOHOOK_galloro_026_large_file_downloads_checker END <<< */
+/* >>> AUTOHOOK_galloro_027_location_registry_checker END <<< */
 };
 
 hook_t native_hooks[] = {
