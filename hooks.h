a9f4cc776d72c3dd6d6180a2d9e942e91e9246cb
CCCethan <rizumu46493@gmail.com>
2026-08-11 03:13:37 +0900

    autohook: galloro_009_cpu_core_count_checker add generated hooks (iter 1)

--- hooks.h
+++ hooks.h
@@ -3930,7 +3930,7 @@ HOOKDEF(DWORD, WINAPI, MapFileAndCheckSumA,
 
 #include "hook_vbscript.h"
 
-/* >>> AUTOHOOK_galloro_007_bios_vendor_registry_checker BEGIN <<< */
+/* >>> AUTOHOOK_galloro_009_cpu_core_count_checker BEGIN <<< */
 HOOKDEF(BOOL, WINAPI, AreFileApisANSI,
 	void
 );
@@ -4283,5 +4283,5 @@ HOOKDEF(BOOL, WINAPI, WriteFile,
 	_Inout_opt_ LPOVERLAPPED lpOverlapped
 );
 
-/* >>> AUTOHOOK_galloro_007_bios_vendor_registry_checker END <<< */
+/* >>> AUTOHOOK_galloro_009_cpu_core_count_checker END <<< */
 
