bd740cc0e89f0e7a5be7bd2b29c082a6423ecfa0
CCCethan <rizumu46493@gmail.com>
2026-08-11 07:08:36 +0900

    autohook: galloro_029_memory_size_checker add generated hooks (iter 1)

--- hook_process.c
+++ hook_process.c
@@ -1540,7 +1540,7 @@ HOOKDEF(BOOL, WINAPI, UpdateProcThreadAttribute,
 	return ret;
 }
 
-/* >>> AUTOHOOK_galloro_029_cpuid_pmu_feature_checker BEGIN <<< */
+/* >>> AUTOHOOK_galloro_029_memory_size_checker BEGIN <<< */
 // -> hook_process.c に追加 | category="process" | winapi:Processes
 HOOKDEF(VOID, WINAPI, ExitProcess, // 呼出規約は WINAPI 仮定(socket/native/CRT系は要確認)
 	_In_ UINT uExitCode
@@ -1742,5 +1742,5 @@ HOOKDEF(BOOL, WINAPI, TlsSetValue, // 呼出規約は WINAPI 仮定(socket/nativ
 	LOQ_bool("process", "ip", "TlsIndex", dwTlsIndex, "TlsValue", lpTlsValue);
 	return ret;
 }
-/* >>> AUTOHOOK_galloro_029_cpuid_pmu_feature_checker END <<< */
+/* >>> AUTOHOOK_galloro_029_memory_size_checker END <<< */
 
