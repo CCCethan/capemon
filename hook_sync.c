8993b3b1b50dc57560954fb57861068c43b28828
CCCethan <rizumu46493@gmail.com>
2026-08-11 09:55:05 +0900

    autohook: galloro_047_system_vs_network_time_checker add generated hooks (iter 1)

--- hook_sync.c
+++ hook_sync.c
@@ -178,7 +178,16 @@ HOOKDEF(NTSTATUS, WINAPI, NtQueryInformationAtom,
 	return ret;
 }
 
-/* >>> AUTOHOOK_galloro_046_sleep_timer_checker_01 BEGIN <<< */
+/* >>> AUTOHOOK_galloro_047_system_vs_network_time_checker BEGIN <<< */
+// -> hook_sync.c に追加 | category="sync" | winapi:Synchronization
+HOOKDEF(VOID, WINAPI, AcquireSRWLockExclusive, // 呼出規約は WINAPI 仮定(socket/native/CRT系は要確認)
+	_Inout_ PSRWLOCK SRWLock
+) {
+	ULONG_PTR ret = 0; (void)ret;  // void 関数: LOQ 用ダミー
+	Old_AcquireSRWLockExclusive(SRWLock);
+	LOQ_void("sync", "P", "SRWLock", SRWLock);
+}
+
 // -> hook_sync.c に追加 | category="sync" | winapi:Synchronization
 HOOKDEF(void, WINAPI, DeleteCriticalSection, // 呼出規約は WINAPI 仮定(socket/native/CRT系は要確認)
 	_Inout_ LPCRITICAL_SECTION lpCriticalSection
@@ -228,5 +237,36 @@ HOOKDEF(void, WINAPI, LeaveCriticalSection, // 呼出規約は WINAPI 仮定(soc
 	Old_LeaveCriticalSection(lpCriticalSection);
 	LOQ_void("sync", "P", "CriticalSection", lpCriticalSection);
 }
-/* >>> AUTOHOOK_galloro_046_sleep_timer_checker_01 END <<< */
+
+// -> hook_sync.c に追加 | category="sync" | winapi:Synchronization
+HOOKDEF(VOID, WINAPI, ReleaseSRWLockExclusive, // 呼出規約は WINAPI 仮定(socket/native/CRT系は要確認)
+	_Inout_ PSRWLOCK SRWLock
+) {
+	ULONG_PTR ret = 0; (void)ret;  // void 関数: LOQ 用ダミー
+	Old_ReleaseSRWLockExclusive(SRWLock);
+	LOQ_void("sync", "P", "SRWLock", SRWLock);
+}
+
+// -> hook_sync.c に追加 | category="sync" | winapi:Synchronization
+HOOKDEF(BOOL, WINAPI, SleepConditionVariableSRW, // 呼出規約は WINAPI 仮定(socket/native/CRT系は要確認)
+	_Inout_ PCONDITION_VARIABLE ConditionVariable,
+	_Inout_ PSRWLOCK SRWLock,
+	_In_ DWORD dwMilliseconds,
+	_In_ ULONG Flags
+) {
+	BOOL ret;
+	ret = Old_SleepConditionVariableSRW(ConditionVariable, SRWLock, dwMilliseconds, Flags);
+	LOQ_bool("sync", "PPii", "ConditionVariable", ConditionVariable, "SRWLock", SRWLock, "Milliseconds", dwMilliseconds, "Flags", Flags);
+	return ret;
+}
+
+// -> hook_sync.c に追加 | category="sync" | winapi:Synchronization
+HOOKDEF(VOID, WINAPI, WakeAllConditionVariable, // 呼出規約は WINAPI 仮定(socket/native/CRT系は要確認)
+	_Inout_ PCONDITION_VARIABLE ConditionVariable
+) {
+	ULONG_PTR ret = 0; (void)ret;  // void 関数: LOQ 用ダミー
+	Old_WakeAllConditionVariable(ConditionVariable);
+	LOQ_void("sync", "P", "ConditionVariable", ConditionVariable);
+}
+/* >>> AUTOHOOK_galloro_047_system_vs_network_time_checker END <<< */
 
--- hooks.h
+++ hooks.h
@@ -3930,9 +3930,26 @@ HOOKDEF(DWORD, WINAPI, MapFileAndCheckSumA,
 
 #include "hook_vbscript.h"
 
-/* >>> AUTOHOOK_galloro_046_sleep_timer_checker_01 BEGIN <<< */
+/* >>> AUTOHOOK_galloro_047_system_vs_network_time_checker BEGIN <<< */
+HOOKDEF(VOID, WINAPI, AcquireSRWLockExclusive,
+	_Inout_ PSRWLOCK SRWLock
+);
+HOOKDEF(BOOL, WINAPI, AreFileApisANSI,
+	void
+);
 HOOKDEF(BOOL, WINAPI, CloseHandle,
 	_In_ HANDLE hObject
+);
+HOOKDEF(int, WINAPI, CompareStringEx,
+	_In_opt_ LPCWSTR lpLocaleName,
+	_In_ DWORD dwCmpFlags,
+	_In_ LPCWSTR lpString1,
+	_In_ int cchCount1,
+	_In_ LPCWSTR lpString2,
+	_In_ int cchCount2,
+	_In_opt_ LPNLSVERSIONINFO lpVersionInformation,
+	_In_opt_ LPVOID lpReserved,
+	_In_opt_ LPARAM lParam
 );
 HOOKDEF(int, WINAPI, CompareStringW,
 	_In_ LCID Locale,
@@ -3956,6 +3973,12 @@ HOOKDEF(void, WINAPI, DeleteCriticalSection,
 );
 HOOKDEF(void, WINAPI, EnterCriticalSection,
 	_Inout_ LPCRITICAL_SECTION lpCriticalSection
+);
+HOOKDEF(BOOL, WINAPI, EnumSystemLocalesEx,
+	_In_ LOCALE_ENUMPROCEX lpLocaleEnumProcEx,
+	_In_ DWORD dwFlags,
+	_In_ LPARAM lParam,
+	_In_opt_ LPVOID lpReserved
 );
 HOOKDEF(BOOL, WINAPI, EnumSystemLocalesW,
 	_In_ LOCALE_ENUMPROC lpLocaleEnumProc,
@@ -3999,6 +4022,15 @@ HOOKDEF(DWORD, WINAPI, GetCurrentProcessId,
 HOOKDEF(DWORD, WINAPI, GetCurrentThreadId,
 	void
 );
+HOOKDEF(int, WINAPI, GetDateFormatEx,
+	_In_opt_ LPCWSTR lpLocaleName,
+	_In_ DWORD dwFlags,
+	_In_opt_ const SYSTEMTIME* lpDate,
+	_In_opt_ LPCWSTR lpFormat,
+	_Out_opt_ LPWSTR lpDateStr,
+	_In_ int cchDate,
+	_In_opt_ LPCWSTR lpCalendar
+);
 HOOKDEF(int, WINAPI, GetDateFormatW,
 	_In_ LCID Locale,
 	_In_ DWORD dwFlags,
@@ -4016,6 +4048,12 @@ HOOKDEF(BOOL, WINAPI, GetFileSizeEx,
 );
 HOOKDEF(DWORD, WINAPI, GetFileType,
 	_In_ HANDLE hFile
+);
+HOOKDEF(int, WINAPI, GetLocaleInfoEx,
+	_In_opt_ LPCWSTR lpLocaleName,
+	_In_ LCTYPE LCType,
+	_Out_opt_ LPWSTR lpLCData,
+	_In_ int cchData
 );
 HOOKDEF(int, WINAPI, GetLocaleInfoW,
 	_In_ LCID Locale,
@@ -4058,6 +4096,14 @@ HOOKDEF(BOOL, WINAPI, GetStringTypeW,
 	_In_ int cchSrc,
 	_Out_ LPWORD lpCharType
 );
+HOOKDEF(int, WINAPI, GetTimeFormatEx,
+	_In_opt_ LPCWSTR lpLocaleName,
+	_In_ DWORD dwFlags,
+	_In_opt_ const SYSTEMTIME* lpTime,
+	_In_opt_ LPCWSTR lpFormat,
+	_Out_opt_ LPWSTR lpTimeStr,
+	_In_ int cchTime
+);
 HOOKDEF(int, WINAPI, GetTimeFormatW,
 	_In_ LCID Locale,
 	_In_ DWORD dwFlags,
@@ -4068,6 +4114,10 @@ HOOKDEF(int, WINAPI, GetTimeFormatW,
 );
 HOOKDEF(DWORD, WINAPI, GetTimeZoneInformation,
 	_Out_ LPTIME_ZONE_INFORMATION lpTimeZoneInformation
+);
+HOOKDEF(int, WINAPI, GetUserDefaultLocaleName,
+	_Out_ LPWSTR lpLocaleName,
+	_In_ int cchLocaleName
 );
 HOOKDEF(LPVOID, WINAPI, HeapAlloc,
 	_In_ HANDLE hHeap,
@@ -4109,6 +4159,15 @@ HOOKDEF(BOOL, WINAPI, IsValidLocale,
 	_In_ LCID Locale,
 	_In_ DWORD dwFlags
 );
+HOOKDEF(BOOL, WINAPI, IsValidLocaleName,
+	_In_ LPCWSTR lpLocaleName
+);
+HOOKDEF(int, WINAPI, LCIDToLocaleName,
+	_In_ LCID Locale,
+	_Out_opt_ LPWSTR lpName,
+	_In_ int cchName,
+	_In_ DWORD dwFlags
+);
 HOOKDEF(int, WINAPI, LCMapStringEx,
 	_In_opt_ LPCWSTR lpLocaleName,
 	_In_ DWORD dwMapFlags,
@@ -4130,6 +4189,10 @@ HOOKDEF(int, WINAPI, LCMapStringW,
 );
 HOOKDEF(void, WINAPI, LeaveCriticalSection,
 	_Inout_ LPCRITICAL_SECTION lpCriticalSection
+);
+HOOKDEF(LCID, WINAPI, LocaleNameToLCID,
+	_In_ LPCWSTR lpName,
+	_In_ DWORD dwFlags
 );
 HOOKDEF(BOOL, WINAPI, QueryPerformanceCounter,
 	_Out_ LARGE_INTEGER* lpPerformanceCount
@@ -4156,6 +4219,9 @@ HOOKDEF(BOOL, WINAPI, ReadFile,
 	_In_ DWORD nNumberOfBytesToRead,
 	_Out_opt_ LPDWORD lpNumberOfBytesRead,
 	_Inout_opt_ LPOVERLAPPED lpOverlapped
+);
+HOOKDEF(VOID, WINAPI, ReleaseSRWLockExclusive,
+	_Inout_ PSRWLOCK SRWLock
 );
 HOOKDEF(PVOID, WINAPI, RtlLookupFunctionEntry,
 	_In_ ULONGLONG ControlPc,
@@ -4192,6 +4258,12 @@ HOOKDEF(BOOL, WINAPI, SetStdHandle,
 HOOKDEF(VOID, WINAPI, Sleep,
 	_In_ DWORD dwMilliseconds
 );
+HOOKDEF(BOOL, WINAPI, SleepConditionVariableSRW,
+	_Inout_ PCONDITION_VARIABLE ConditionVariable,
+	_Inout_ PSRWLOCK SRWLock,
+	_In_ DWORD dwMilliseconds,
+	_In_ ULONG Flags
+);
 HOOKDEF(BOOL, WINAPI, TerminateProcess,
 	_In_ HANDLE hProcess,
 	_In_ UINT uExitCode
@@ -4215,6 +4287,9 @@ HOOKDEF(BOOL, WINAPI, VirtualProtect,
 	_In_ DWORD flNewProtect,
 	_Out_ PDWORD lpflOldProtect
 );
+HOOKDEF(VOID, WINAPI, WakeAllConditionVariable,
+	_Inout_ PCONDITION_VARIABLE ConditionVariable
+);
 HOOKDEF(BOOL, WINAPI, WriteFile,
 	_In_ HANDLE hFile,
 	_In_ LPCVOID lpBuffer,
@@ -4223,5 +4298,5 @@ HOOKDEF(BOOL, WINAPI, WriteFile,
 	_Inout_opt_ LPOVERLAPPED lpOverlapped
 );
 
-/* >>> AUTOHOOK_galloro_046_sleep_timer_checker_01 END <<< */
+/* >>> AUTOHOOK_galloro_047_system_vs_network_time_checker END <<< */
 
