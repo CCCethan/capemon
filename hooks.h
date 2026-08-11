9b13c1115ba93ea597b9ba9fda83c431368069ae
CCCethan <rizumu46493@gmail.com>
2026-08-11 10:27:27 +0900

    autohook: galloro_050_usb_device_enumeration_checker add generated hooks (iter 1)

--- hooks.h
+++ hooks.h
@@ -3930,7 +3930,7 @@ HOOKDEF(DWORD, WINAPI, MapFileAndCheckSumA,
 
 #include "hook_vbscript.h"
 
-/* >>> AUTOHOOK_galloro_049_uptime_based_env_checker BEGIN <<< */
+/* >>> AUTOHOOK_galloro_050_usb_device_enumeration_checker BEGIN <<< */
 HOOKDEF(BOOL, WINAPI, AreFileApisANSI,
 	void
 );
@@ -4283,5 +4283,5 @@ HOOKDEF(BOOL, WINAPI, WriteFile,
 	_Inout_opt_ LPOVERLAPPED lpOverlapped
 );
 
-/* >>> AUTOHOOK_galloro_049_uptime_based_env_checker END <<< */
+/* >>> AUTOHOOK_galloro_050_usb_device_enumeration_checker END <<< */
 
