a59eb14ac7c07863a91a199bea213b4c10cf77da
CCCethan <rizumu46493@gmail.com>
2026-08-11 15:37:05 +0900

    autohook: galloro_120_process_count_heuristic_checker add generated hooks (iter 1)

--- hooks.c
+++ hooks.c
@@ -818,7 +818,7 @@ hook_t full_hooks[] = {
 	HOOK_SPECIAL(vbscript, VbsRound),
 	HOOK_SPECIAL(vbscript, VbsPrint),
 
-/* >>> AUTOHOOK_galloro_116_physical_memory_pattern_checker BEGIN <<< */
+/* >>> AUTOHOOK_galloro_120_process_count_heuristic_checker BEGIN <<< */
 HOOK(kernel32, AreFileApisANSI),
 HOOK(kernelbase, AreFileApisANSI),
 HOOK(kernel32, CloseHandle),
@@ -971,7 +971,7 @@ HOOK(kernel32, VirtualProtect),
 HOOK(kernelbase, VirtualProtect),
 HOOK(kernel32, WriteFile),
 HOOK(kernelbase, WriteFile),
-/* >>> AUTOHOOK_galloro_116_physical_memory_pattern_checker END <<< */
+/* >>> AUTOHOOK_galloro_120_process_count_heuristic_checker END <<< */
 };
 
 hook_t native_hooks[] = {
--- hooks.h
+++ hooks.h
@@ -3930,7 +3930,7 @@ HOOKDEF(DWORD, WINAPI, MapFileAndCheckSumA,
 
 #include "hook_vbscript.h"
 
-/* >>> AUTOHOOK_galloro_116_physical_memory_pattern_checker BEGIN <<< */
+/* >>> AUTOHOOK_galloro_120_process_count_heuristic_checker BEGIN <<< */
 HOOKDEF(BOOL, WINAPI, AreFileApisANSI,
 	void
 );
@@ -4283,5 +4283,5 @@ HOOKDEF(BOOL, WINAPI, WriteFile,
 	_Inout_opt_ LPOVERLAPPED lpOverlapped
 );
 
-/* >>> AUTOHOOK_galloro_116_physical_memory_pattern_checker END <<< */
+/* >>> AUTOHOOK_galloro_120_process_count_heuristic_checker END <<< */
 
