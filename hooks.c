f7a0f5a00dbd7dacf44f6cc8d8742467c7ebfc9a
CCCethan <rizumu46493@gmail.com>
2026-08-13 06:49:17 +0900

    autohook: mitre_084_recent_apps_usage_checker add generated hooks (iter 1)

--- hooks.c
+++ hooks.c
@@ -818,7 +818,7 @@ hook_t full_hooks[] = {
 	HOOK_SPECIAL(vbscript, VbsRound),
 	HOOK_SPECIAL(vbscript, VbsPrint),
 
-/* >>> AUTOHOOK_mitre_083_rdtsc_qpc_ratio_checker BEGIN <<< */
+/* >>> AUTOHOOK_mitre_084_recent_apps_usage_checker BEGIN <<< */
 HOOK(kernel32, AreFileApisANSI),
 HOOK(kernelbase, AreFileApisANSI),
 HOOK(kernel32, CloseHandle),
@@ -971,7 +971,7 @@ HOOK(kernel32, VirtualProtect),
 HOOK(kernelbase, VirtualProtect),
 HOOK(kernel32, WriteFile),
 HOOK(kernelbase, WriteFile),
-/* >>> AUTOHOOK_mitre_083_rdtsc_qpc_ratio_checker END <<< */
+/* >>> AUTOHOOK_mitre_084_recent_apps_usage_checker END <<< */
 };
 
 hook_t native_hooks[] = {
--- hooks.h
+++ hooks.h
@@ -3930,7 +3930,7 @@ HOOKDEF(DWORD, WINAPI, MapFileAndCheckSumA,
 
 #include "hook_vbscript.h"
 
-/* >>> AUTOHOOK_mitre_083_rdtsc_qpc_ratio_checker BEGIN <<< */
+/* >>> AUTOHOOK_mitre_084_recent_apps_usage_checker BEGIN <<< */
 HOOKDEF(BOOL, WINAPI, AreFileApisANSI,
 	void
 );
@@ -4283,5 +4283,5 @@ HOOKDEF(BOOL, WINAPI, WriteFile,
 	_Inout_opt_ LPOVERLAPPED lpOverlapped
 );
 
-/* >>> AUTOHOOK_mitre_083_rdtsc_qpc_ratio_checker END <<< */
+/* >>> AUTOHOOK_mitre_084_recent_apps_usage_checker END <<< */
 
