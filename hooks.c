a6825fa09c851efb907219bd16ee3dad2d1973bd
CCCethan <rizumu46493@gmail.com>
2026-08-13 06:10:02 +0900

    autohook: mitre_081_quick_access_size_analysis add generated hooks (iter 1)

--- hooks.c
+++ hooks.c
@@ -818,7 +818,7 @@ hook_t full_hooks[] = {
 	HOOK_SPECIAL(vbscript, VbsRound),
 	HOOK_SPECIAL(vbscript, VbsPrint),
 
-/* >>> AUTOHOOK_mitre_076_process_cpu_wall_ratio_checker BEGIN <<< */
+/* >>> AUTOHOOK_mitre_081_quick_access_size_analysis BEGIN <<< */
 HOOK(kernel32, AreFileApisANSI),
 HOOK(kernelbase, AreFileApisANSI),
 HOOK(kernel32, CloseHandle),
@@ -867,6 +867,10 @@ HOOK(kernel32, GetDateFormatW),
 HOOK(kernelbase, GetDateFormatW),
 HOOK(kernel32, GetEnvironmentStringsW),
 HOOK(kernelbase, GetEnvironmentStringsW),
+HOOK(kernel32, GetEnvironmentVariableW),
+HOOK(kernelbase, GetEnvironmentVariableW),
+HOOK(kernel32, GetFileAttributesExW),
+HOOK(kernelbase, GetFileAttributesExW),
 HOOK(kernel32, GetFileSizeEx),
 HOOK(kernelbase, GetFileSizeEx),
 HOOK(kernel32, GetFileType),
@@ -887,8 +891,6 @@ HOOK(kernel32, GetProcAddress),
 HOOK(kernelbase, GetProcAddress),
 HOOK(kernel32, GetProcessHeap),
 HOOK(kernelbase, GetProcessHeap),
-HOOK(kernel32, GetProcessTimes),
-HOOK(kernelbase, GetProcessTimes),
 HOOK(kernel32, GetStartupInfoW),
 HOOK(kernelbase, GetStartupInfoW),
 HOOK(kernel32, GetStdHandle),
@@ -973,7 +975,7 @@ HOOK(kernel32, VirtualProtect),
 HOOK(kernelbase, VirtualProtect),
 HOOK(kernel32, WriteFile),
 HOOK(kernelbase, WriteFile),
-/* >>> AUTOHOOK_mitre_076_process_cpu_wall_ratio_checker END <<< */
+/* >>> AUTOHOOK_mitre_081_quick_access_size_analysis END <<< */
 };
 
 hook_t native_hooks[] = {
--- hooks.h
+++ hooks.h
@@ -3930,7 +3930,7 @@ HOOKDEF(DWORD, WINAPI, MapFileAndCheckSumA,
 
 #include "hook_vbscript.h"
 
-/* >>> AUTOHOOK_mitre_076_process_cpu_wall_ratio_checker BEGIN <<< */
+/* >>> AUTOHOOK_mitre_081_quick_access_size_analysis BEGIN <<< */
 HOOKDEF(BOOL, WINAPI, AreFileApisANSI,
 	void
 );
@@ -4039,6 +4039,16 @@ HOOKDEF(int, WINAPI, GetDateFormatW,
 HOOKDEF(LPWSTR, WINAPI, GetEnvironmentStringsW,
 	void
 );
+HOOKDEF(DWORD, WINAPI, GetEnvironmentVariableW,
+	_In_opt_ LPCWSTR lpName,
+	_Out_opt_ LPWSTR lpBuffer,
+	_In_ DWORD nSize
+);
+HOOKDEF(BOOL, WINAPI, GetFileAttributesExW,
+	_In_ LPCWSTR lpFileName,
+	_In_ GET_FILEEX_INFO_LEVELS fInfoLevelId,
+	_Out_ LPVOID lpFileInformation
+);
 HOOKDEF(BOOL, WINAPI, GetFileSizeEx,
 	_In_ HANDLE hFile,
 	_Out_ PLARGE_INTEGER lpFileSize
@@ -4080,13 +4090,6 @@ HOOKDEF(FARPROC, WINAPI, GetProcAddress,
 );
 HOOKDEF(HANDLE, WINAPI, GetProcessHeap,
 	void
-);
-HOOKDEF(BOOL, WINAPI, GetProcessTimes,
-	_In_ HANDLE hProcess,
-	_Out_ LPFILETIME lpCreationTime,
-	_Out_ LPFILETIME lpExitTime,
-	_Out_ LPFILETIME lpKernelTime,
-	_Out_ LPFILETIME lpUserTime
 );
 HOOKDEF(VOID, WINAPI, GetStartupInfoW,
 	_Out_ LPSTARTUPINFOW lpStartupInfo
@@ -4290,5 +4293,5 @@ HOOKDEF(BOOL, WINAPI, WriteFile,
 	_Inout_opt_ LPOVERLAPPED lpOverlapped
 );
 
-/* >>> AUTOHOOK_mitre_076_process_cpu_wall_ratio_checker END <<< */
+/* >>> AUTOHOOK_mitre_081_quick_access_size_analysis END <<< */
 
