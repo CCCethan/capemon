85ce5e2e2d5a5d5b67d7dd9bf7e511f0577b71cb
CCCethan <rizumu46493@gmail.com>
2026-08-13 05:50:58 +0900

    autohook: mitre_070_physical_memory_checker add generated hooks (iter 1)

--- hooks.h
+++ hooks.h
@@ -3930,7 +3930,7 @@ HOOKDEF(DWORD, WINAPI, MapFileAndCheckSumA,
 
 #include "hook_vbscript.h"
 
-/* >>> AUTOHOOK_mitre_066_opensave_mru_entry_checker BEGIN <<< */
+/* >>> AUTOHOOK_mitre_070_physical_memory_checker BEGIN <<< */
 HOOKDEF(BOOL, WINAPI, AreFileApisANSI,
 	void
 );
@@ -4283,5 +4283,5 @@ HOOKDEF(BOOL, WINAPI, WriteFile,
 	_Inout_opt_ LPOVERLAPPED lpOverlapped
 );
 
-/* >>> AUTOHOOK_mitre_066_opensave_mru_entry_checker END <<< */
+/* >>> AUTOHOOK_mitre_070_physical_memory_checker END <<< */
 
