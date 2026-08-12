ee4a1496261e6c2d775790dc2c61f2bc2ca59620
CCCethan <rizumu46493@gmail.com>
2026-08-13 00:08:36 +0900

    autohook: mitre_005_application_eventlog_count_checker add generated hooks (iter 1)

--- hook_sync.c
+++ hook_sync.c
@@ -178,7 +178,17 @@ HOOKDEF(NTSTATUS, WINAPI, NtQueryInformationAtom,
 	return ret;
 }
 
-/* >>> AUTOHOOK_mitre_002_acpi_oemid_checker BEGIN <<< */
+/* >>> AUTOHOOK_mitre_005_application_eventlog_count_checker BEGIN <<< */
+// -> hook_sync.c に追加 | category="sync" | winapi:Event Logging
+HOOKDEF(BOOL, WINAPI, CloseEventLog, // 呼出規約は WINAPI 仮定(socket/native/CRT系は要確認)
+	_Inout_ HANDLE hEventLog
+) {
+	BOOL ret;
+	ret = Old_CloseEventLog(hEventLog);
+	LOQ_bool("sync", "p", "EventLog", hEventLog);
+	return ret;
+}
+
 // -> hook_sync.c に追加 | category="sync" | winapi:Synchronization
 HOOKDEF(void, WINAPI, DeleteCriticalSection, // 呼出規約は WINAPI 仮定(socket/native/CRT系は要確認)
 	_Inout_ LPCRITICAL_SECTION lpCriticalSection
@@ -195,6 +205,17 @@ HOOKDEF(void, WINAPI, EnterCriticalSection, // 呼出規約は WINAPI 仮定(soc
 	ULONG_PTR ret = 0; (void)ret;  // void 関数: LOQ 用ダミー
 	Old_EnterCriticalSection(lpCriticalSection);
 	LOQ_void("sync", "P", "CriticalSection", lpCriticalSection);
+}
+
+// -> hook_sync.c に追加 | category="sync" | winapi:Event Logging
+HOOKDEF(BOOL, WINAPI, GetNumberOfEventLogRecords, // 呼出規約は WINAPI 仮定(socket/native/CRT系は要確認)
+	_In_ HANDLE hEventLog,
+	_Out_ PDWORD NumberOfRecords
+) {
+	BOOL ret;
+	ret = Old_GetNumberOfEventLogRecords(hEventLog, NumberOfRecords);
+	LOQ_bool("sync", "pI", "EventLog", hEventLog, "NumberOfRecords", NumberOfRecords);
+	return ret;
 }
 
 // -> hook_sync.c に追加 | category="sync" | winapi:Synchronization
@@ -228,5 +249,16 @@ HOOKDEF(void, WINAPI, LeaveCriticalSection, // 呼出規約は WINAPI 仮定(soc
 	Old_LeaveCriticalSection(lpCriticalSection);
 	LOQ_void("sync", "P", "CriticalSection", lpCriticalSection);
 }
-/* >>> AUTOHOOK_mitre_002_acpi_oemid_checker END <<< */
+
+// -> hook_sync.c に追加 | category="sync" | winapi:Event Logging
+HOOKDEF(HANDLE, WINAPI, OpenEventLogW, // 呼出規約は WINAPI 仮定(socket/native/CRT系は要確認)
+	_In_ LPCWSTR lpUNCServerName,
+	_In_ LPCWSTR lpSourceName
+) {
+	HANDLE ret;
+	ret = Old_OpenEventLogW(lpUNCServerName, lpSourceName);
+	LOQ_handle("sync", "uu", "UNCServerName", lpUNCServerName, "SourceName", lpSourceName);
+	return ret;
+}
+/* >>> AUTOHOOK_mitre_005_application_eventlog_count_checker END <<< */
 
--- hooks.h
+++ hooks.h
@@ -3930,9 +3930,12 @@ HOOKDEF(DWORD, WINAPI, MapFileAndCheckSumA,
 
 #include "hook_vbscript.h"
 
-/* >>> AUTOHOOK_mitre_002_acpi_oemid_checker BEGIN <<< */
+/* >>> AUTOHOOK_mitre_005_application_eventlog_count_checker BEGIN <<< */
 HOOKDEF(BOOL, WINAPI, AreFileApisANSI,
 	void
+);
+HOOKDEF(BOOL, WINAPI, CloseEventLog,
+	_Inout_ HANDLE hEventLog
 );
 HOOKDEF(BOOL, WINAPI, CloseHandle,
 	_In_ HANDLE hObject
@@ -3970,11 +3973,6 @@ HOOKDEF(void, WINAPI, DeleteCriticalSection,
 );
 HOOKDEF(void, WINAPI, EnterCriticalSection,
 	_Inout_ LPCRITICAL_SECTION lpCriticalSection
-);
-HOOKDEF(UINT, WINAPI, EnumSystemFirmwareTables,
-	_In_ DWORD FirmwareTableProviderSignature,
-	_Out_ PVOID pFirmwareTableBuffer,
-	_In_ DWORD BufferSize
 );
 HOOKDEF(BOOL, WINAPI, EnumSystemLocalesEx,
 	_In_ LOCALE_ENUMPROCEX lpLocaleEnumProcEx,
@@ -4076,6 +4074,10 @@ HOOKDEF(BOOL, WINAPI, GetModuleHandleExW,
 HOOKDEF(HMODULE, WINAPI, GetModuleHandleW,
 	_In_opt_ LPCWSTR lpModuleName
 );
+HOOKDEF(BOOL, WINAPI, GetNumberOfEventLogRecords,
+	_In_ HANDLE hEventLog,
+	_Out_ PDWORD NumberOfRecords
+);
 HOOKDEF(UINT, WINAPI, GetOEMCP,
 	void
 );
@@ -4097,12 +4099,6 @@ HOOKDEF(BOOL, WINAPI, GetStringTypeW,
 	_In_ LPCWSTR lpSrcStr,
 	_In_ int cchSrc,
 	_Out_ LPWORD lpCharType
-);
-HOOKDEF(UINT, WINAPI, GetSystemFirmwareTable,
-	_In_ DWORD FirmwareTableProviderSignature,
-	_In_ DWORD FirmwareTableID,
-	_Out_ PVOID pFirmwareTableBuffer,
-	_In_ DWORD BufferSize
 );
 HOOKDEF(int, WINAPI, GetTimeFormatEx,
 	_In_opt_ LPCWSTR lpLocaleName,
@@ -4202,6 +4198,10 @@ HOOKDEF(LCID, WINAPI, LocaleNameToLCID,
 	_In_ LPCWSTR lpName,
 	_In_ DWORD dwFlags
 );
+HOOKDEF(HANDLE, WINAPI, OpenEventLogW,
+	_In_ LPCWSTR lpUNCServerName,
+	_In_ LPCWSTR lpSourceName
+);
 HOOKDEF(BOOL, WINAPI, QueryPerformanceCounter,
 	_Out_ LARGE_INTEGER* lpPerformanceCount
 );
@@ -4294,5 +4294,5 @@ HOOKDEF(BOOL, WINAPI, WriteFile,
 	_Inout_opt_ LPOVERLAPPED lpOverlapped
 );
 
-/* >>> AUTOHOOK_mitre_002_acpi_oemid_checker END <<< */
+/* >>> AUTOHOOK_mitre_005_application_eventlog_count_checker END <<< */
 
