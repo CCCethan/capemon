8b48882b8a712682faf7a533cad0ac7d3ed066f8
CCCethan <rizumu46493@gmail.com>
2026-08-13 10:20:18 +0900

    autohook: mitre_129_window_interaction_monitor add generated hooks (iter 3)

--- hooks.h
+++ hooks.h
@@ -3930,16 +3930,54 @@ HOOKDEF(DWORD, WINAPI, MapFileAndCheckSumA,
 
 #include "hook_vbscript.h"
 
-/* >>> AUTOHOOK_mitre_046_jumplist_age_evaluator BEGIN <<< */
-HOOKDEF(LONG, WINAPI, CompareFileTime,
-	_In_ const FILETIME* lpFileTime1,
-	_In_ const FILETIME* lpFileTime2
+/* >>> AUTOHOOK_mitre_129_window_interaction_monitor BEGIN <<< */
+HOOKDEF(LRESULT, WINAPI, DefWindowProcW,
+	_In_ HWND hWnd,
+	_In_ UINT Msg,
+	_In_ WPARAM wParam,
+	_In_ LPARAM lParam
+);
+HOOKDEF(BOOL, WINAPI, DestroyWindow,
+	_In_ HWND hWnd
+);
+HOOKDEF(LONG_PTR, WINAPI, GetWindowLongPtrW,
+	_In_ HWND hWnd,
+	_In_ int nIndex
+);
+HOOKDEF(HCURSOR, WINAPI, LoadCursorW,
+	_In_opt_ HINSTANCE hInstance,
+	_In_ LPCWSTR lpCursorName
+);
+HOOKDEF(HICON, WINAPI, LoadIconW,
+	_In_opt_ HINSTANCE hInstance,
+	_In_ LPCWSTR lpIconName
+);
+HOOKDEF(ATOM, WINAPI, RegisterClassExW,
+	_In_ const WNDCLASSEXW* lpwcx
+);
+HOOKDEF(BOOL, WINAPI, ShowWindow,
+	_In_ HWND hWnd,
+	_In_ int nCmdShow
+);
+HOOKDEF(BOOL, WINAPI, UpdateWindow,
+	_In_ HWND hWnd
+);
+HOOKDEF(BOOL, WINAPI, AreFileApisANSI,
+	void
 );
 HOOKDEF(BOOL, WINAPI, CloseHandle,
 	_In_ HANDLE hObject
 );
-HOOKDEF(void, WINAPI, CoTaskMemFree,
-	_In_opt_ LPVOID pv
+HOOKDEF(int, WINAPI, CompareStringEx,
+	_In_opt_ LPCWSTR lpLocaleName,
+	_In_ DWORD dwCmpFlags,
+	_In_ LPCWSTR lpString1,
+	_In_ int cchCount1,
+	_In_ LPCWSTR lpString2,
+	_In_ int cchCount2,
+	_In_opt_ LPNLSVERSIONINFO lpVersionInformation,
+	_In_opt_ LPVOID lpReserved,
+	_In_opt_ LPARAM lParam
 );
 HOOKDEF(int, WINAPI, CompareStringW,
 	_In_ LCID Locale,
@@ -3961,8 +3999,17 @@ HOOKDEF(HANDLE, WINAPI, CreateFileW,
 HOOKDEF(void, WINAPI, DeleteCriticalSection,
 	_Inout_ LPCRITICAL_SECTION lpCriticalSection
 );
+HOOKDEF(LRESULT, WINAPI, DispatchMessageW,
+	_In_ const MSG* lpmsg
+);
 HOOKDEF(void, WINAPI, EnterCriticalSection,
 	_Inout_ LPCRITICAL_SECTION lpCriticalSection
+);
+HOOKDEF(BOOL, WINAPI, EnumSystemLocalesEx,
+	_In_ LOCALE_ENUMPROCEX lpLocaleEnumProcEx,
+	_In_ DWORD dwFlags,
+	_In_ LPARAM lParam,
+	_In_opt_ LPVOID lpReserved
 );
 HOOKDEF(BOOL, WINAPI, EnumSystemLocalesW,
 	_In_ LOCALE_ENUMPROC lpLocaleEnumProc,
@@ -3971,16 +4018,8 @@ HOOKDEF(BOOL, WINAPI, EnumSystemLocalesW,
 HOOKDEF(VOID, WINAPI, ExitProcess,
 	_In_ UINT uExitCode
 );
-HOOKDEF(BOOL, WINAPI, FileTimeToSystemTime,
-	_In_ const FILETIME* lpFileTime,
-	_Out_ LPSYSTEMTIME lpSystemTime
-);
 HOOKDEF(BOOL, WINAPI, FindClose,
 	_Inout_ HANDLE hFindFile
-);
-HOOKDEF(HANDLE, WINAPI, FindFirstFileW,
-	_In_ LPCWSTR lpFileName,
-	_Out_ LPWIN32_FIND_DATAW lpFindFileData
 );
 HOOKDEF(BOOL, WINAPI, FlushFileBuffers,
 	_In_ HANDLE hFile
@@ -4014,6 +4053,15 @@ HOOKDEF(DWORD, WINAPI, GetCurrentProcessId,
 HOOKDEF(DWORD, WINAPI, GetCurrentThreadId,
 	void
 );
+HOOKDEF(int, WINAPI, GetDateFormatEx,
+	_In_opt_ LPCWSTR lpLocaleName,
+	_In_ DWORD dwFlags,
+	_In_opt_ const SYSTEMTIME* lpDate,
+	_In_opt_ LPCWSTR lpFormat,
+	_Out_opt_ LPWSTR lpDateStr,
+	_In_ int cchDate,
+	_In_opt_ LPCWSTR lpCalendar
+);
 HOOKDEF(int, WINAPI, GetDateFormatW,
 	_In_ LCID Locale,
 	_In_ DWORD dwFlags,
@@ -4031,6 +4079,12 @@ HOOKDEF(BOOL, WINAPI, GetFileSizeEx,
 );
 HOOKDEF(DWORD, WINAPI, GetFileType,
 	_In_ HANDLE hFile
+);
+HOOKDEF(int, WINAPI, GetLocaleInfoEx,
+	_In_opt_ LPCWSTR lpLocaleName,
+	_In_ LCTYPE LCType,
+	_Out_opt_ LPWSTR lpLCData,
+	_In_ int cchData
 );
 HOOKDEF(int, WINAPI, GetLocaleInfoW,
 	_In_ LCID Locale,
@@ -4073,6 +4127,14 @@ HOOKDEF(BOOL, WINAPI, GetStringTypeW,
 	_In_ int cchSrc,
 	_Out_ LPWORD lpCharType
 );
+HOOKDEF(int, WINAPI, GetTimeFormatEx,
+	_In_opt_ LPCWSTR lpLocaleName,
+	_In_ DWORD dwFlags,
+	_In_opt_ const SYSTEMTIME* lpTime,
+	_In_opt_ LPCWSTR lpFormat,
+	_Out_opt_ LPWSTR lpTimeStr,
+	_In_ int cchTime
+);
 HOOKDEF(int, WINAPI, GetTimeFormatW,
 	_In_ LCID Locale,
 	_In_ DWORD dwFlags,
@@ -4083,6 +4145,10 @@ HOOKDEF(int, WINAPI, GetTimeFormatW,
 );
 HOOKDEF(DWORD, WINAPI, GetTimeZoneInformation,
 	_Out_ LPTIME_ZONE_INFORMATION lpTimeZoneInformation
+);
+HOOKDEF(int, WINAPI, GetUserDefaultLocaleName,
+	_Out_ LPWSTR lpLocaleName,
+	_In_ int cchLocaleName
 );
 HOOKDEF(LPVOID, WINAPI, HeapAlloc,
 	_In_ HANDLE hHeap,
@@ -4124,6 +4190,15 @@ HOOKDEF(BOOL, WINAPI, IsValidLocale,
 	_In_ LCID Locale,
 	_In_ DWORD dwFlags
 );
+HOOKDEF(BOOL, WINAPI, IsValidLocaleName,
+	_In_ LPCWSTR lpLocaleName
+);
+HOOKDEF(int, WINAPI, LCIDToLocaleName,
+	_In_ LCID Locale,
+	_Out_opt_ LPWSTR lpName,
+	_In_ int cchName,
+	_In_ DWORD dwFlags
+);
 HOOKDEF(int, WINAPI, LCMapStringEx,
 	_In_opt_ LPCWSTR lpLocaleName,
 	_In_ DWORD dwMapFlags,
@@ -4145,6 +4220,20 @@ HOOKDEF(int, WINAPI, LCMapStringW,
 );
 HOOKDEF(void, WINAPI, LeaveCriticalSection,
 	_Inout_ LPCRITICAL_SECTION lpCriticalSection
+);
+HOOKDEF(LCID, WINAPI, LocaleNameToLCID,
+	_In_ LPCWSTR lpName,
+	_In_ DWORD dwFlags
+);
+HOOKDEF(BOOL, WINAPI, PeekMessageW,
+	_Out_ LPMSG lpMsg,
+	_In_opt_ HWND hWnd,
+	_In_ UINT wMsgFilterMin,
+	_In_ UINT wMsgFilterMax,
+	_In_ UINT wRemoveMsg
+);
+HOOKDEF(VOID, WINAPI, PostQuitMessage,
+	_In_ int nExitCode
 );
 HOOKDEF(BOOL, WINAPI, QueryPerformanceCounter,
 	_Out_ LARGE_INTEGER* lpPerformanceCount
@@ -4224,6 +4313,9 @@ HOOKDEF(BOOL, WINAPI, TlsSetValue,
 	_In_ DWORD dwTlsIndex,
 	_In_opt_ LPVOID lpTlsValue
 );
+HOOKDEF(BOOL, WINAPI, TranslateMessage,
+	_In_ const MSG* lpMsg
+);
 HOOKDEF(BOOL, WINAPI, VirtualProtect,
 	_In_ LPVOID lpAddress,
 	_In_ SIZE_T dwSize,
@@ -4238,5 +4330,5 @@ HOOKDEF(BOOL, WINAPI, WriteFile,
 	_Inout_opt_ LPOVERLAPPED lpOverlapped
 );
 
-/* >>> AUTOHOOK_mitre_046_jumplist_age_evaluator END <<< */
+/* >>> AUTOHOOK_mitre_129_window_interaction_monitor END <<< */
 
